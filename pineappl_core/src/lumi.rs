fa881bce64e84ec1ce8089663946aba7bf3eb103
Christopher Schwan <handgranaten-herbert@posteo.de>
2020-05-01 10:50:01 +0200

    Improve lumi functionality

--- pineappl_core/src/lumi.rs
+++ pineappl_core/src/lumi.rs
@@ -1,18 +1,21 @@
-//! Module containing all structures for creating luminosity functions.
+//! Module for everything related to luminosity functions.
 
-/// Structure representing an entry of a luminosity function. Each entry consists of a tuple, which
-/// contains, in the following order, the PDG id of the first incoming parton, then the PDG id of
-/// the second parton and finally a numerical factor that will multiply the result for this
-/// specific combination
-#[derive(Debug, PartialEq)]
+/// This structure represens an entry of a luminosity function. Each entry consists of a tuple,
+/// which contains, in the following order, the PDG id of the first incoming parton, then the PDG
+/// id of the second parton, and finally a numerical factor that will multiply the result for this
+/// specific combination.
+#[derive(Clone, Debug, PartialEq)]
 pub struct LumiEntry {
     entry: Vec<(i32, i32, f64)>,
 }
 
 impl LumiEntry {
-    /// Constructor for LumiEntry.
-    pub fn new(entry: Vec<(i32, i32, f64)>) -> LumiEntry {
-        LumiEntry { entry }
+    /// Constructor for LumiEntry. Note that `entry` must be non-empty, otherwise this function
+    /// panics.
+    pub fn new(entry: Vec<(i32, i32, f64)>) -> Self {
+        assert!(!entry.is_empty());
+
+        Self { entry }
     }
 }
 
@@ -35,14 +38,20 @@ macro_rules! lumi_entry {
 
 /// Structure implementing a luminosity function. Each luminosity function is collection of
 /// LumiEntries.
+#[derive(Clone, Default)]
 pub struct Lumi {
     tuples: Vec<LumiEntry>,
 }
 
 impl Lumi {
     /// Constructor for Lumi.
-    pub fn new(tuples: Vec<LumiEntry>) -> Lumi {
-        Lumi { tuples }
+    pub fn new(tuples: Vec<LumiEntry>) -> Self {
+        Self { tuples }
+    }
+
+    /// Add a new `LumiEntry` to the luminosity function.
+    pub fn add(&mut self, entry: LumiEntry) {
+        self.tuples.push(entry);
     }
 
     /// Returns the number of LumiEntries in the Lumi object.
@@ -57,16 +66,29 @@ impl Lumi {
 }
 
 #[cfg(test)]
-mod test {
+mod tests {
     use super::*;
 
     #[test]
-    fn test() {
+    fn lumi_new() {
         let lumi = Lumi::new(vec![
             lumi_entry![2, 2, 1.0; 4, 4, 1.0],
             lumi_entry![1, 1, 1.0; 3, 3, 1.0],
             lumi_entry![5, 5, 1.0],
         ]);
+        assert!(!lumi.is_empty());
         assert_eq!(lumi.len(), 3);
+    }
+
+    #[test]
+    fn lumi_add() {
+        let mut lumi = Lumi::new(vec![
+            lumi_entry![2, 2, 1.0; 4, 4, 1.0],
+            lumi_entry![1, 1, 1.0; 3, 3, 1.0],
+            lumi_entry![5, 5, 1.0],
+        ]);
+
+        lumi.add(lumi_entry![2, -2, 1.0; 4, -4, 1.0]);
+        assert_eq!(lumi.len(), 4);
     }
 }
