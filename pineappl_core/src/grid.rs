6f0642eae9fb38a83df7e04c9764c17ff803b2f3
Christopher Schwan <handgranaten-herbert@posteo.de>
2020-05-01 10:56:48 +0200

    Restructure `Grid`
    - `Grid` is now a struct, containing `Subgrid`s, which are trait
      objects

--- pineappl_core/src/grid.rs
+++ pineappl_core/src/grid.rs
@@ -1,7 +1,11 @@
 //! Module containing all traits and supporting structures for grids.
 
+use super::bin::BinLimits;
+use super::lumi::Lumi;
+use super::ntuple_grid::NtupleSubgrid;
+
 /// Structure for the metadata of a subgrid.
-pub struct Subgrid {
+pub struct SubgridData {
     /// Exponent of the strong coupling.
     pub alphas: u32,
     /// Exponent of the electromagnetic coupling.
@@ -12,19 +16,35 @@ pub struct Subgrid {
     pub logxif: u32,
 }
 
-/// Trait each grid must implement.
-pub trait Grid {
+/// Trait each subgrid must implement.
+pub trait Subgrid {
     /// Fills the grid with events for the parton momentum fractions `x1` and `x2`, the scale `q2`,
-    /// the observable `obs`. The events are stored in `weights` and must be ordered as the
-    /// corresponding luminosity function was created. The weights are filled into the subgrid with
-    /// the given index.
-    fn fill(&mut self, x1: f64, x2: f64, q2: f64, obs: f64, weights: &[f64], subgrid: usize);
+    /// and the observable at the `obs_index`. The events are stored in `weights` and must be
+    /// ordered as the corresponding luminosity function was created.
+    fn fill(&mut self, x1: f64, x2: f64, q2: f64, obs_index: usize, weights: &[f64]);
 }
 
-#[cfg(test)]
-mod test {
-    //use super::*;
+/// A collection of subgrids.
+pub struct Grid {
+    subgrids: Vec<Box<dyn Subgrid>>,
+    lumi: Lumi,
+    bin_limits: BinLimits,
+}
 
-    #[test]
-    fn test() {}
+impl Grid {
+    /// Constructor.
+    pub fn new(lumi: Lumi, subgrid_data: Vec<SubgridData>, bin_limits: Vec<f64>) -> Self {
+        Self {
+            subgrids: subgrid_data
+                .into_iter()
+                .map(|d| {
+                    let result: Box<dyn Subgrid> =
+                        Box::new(NtupleSubgrid::new(lumi.len(), bin_limits.len() - 1, d));
+                    result
+                })
+                .collect::<Vec<Box<dyn Subgrid>>>(),
+            lumi,
+            bin_limits: BinLimits::new(bin_limits),
+        }
+    }
 }
--- pineappl_core/src/ntuple_grid.rs
+++ pineappl_core/src/ntuple_grid.rs
@@ -1,9 +1,6 @@
 //! Provides an implementation of the `Grid` trait with n-tuples.
 
-use super::bin::BinLimits;
-use super::grid::Grid;
-use super::grid::Subgrid;
-use super::lumi::Lumi;
+use super::grid::{Subgrid, SubgridData};
 
 #[derive(Clone, Debug, PartialEq)]
 struct Ntuple {
@@ -20,95 +17,70 @@ impl Ntuple {
 }
 
 /// Structure holding a grid with an n-tuple as the storage method for weights.
-pub struct NtupleGrid {
-    ntuples: Vec<Vec<Vec<Vec<Ntuple>>>>,
-    lumi: Lumi,
-    subgrids: Vec<Subgrid>,
-    bin_limits: BinLimits,
+pub struct NtupleSubgrid {
+    ntuples: Vec<Vec<Vec<Ntuple>>>,
+    subgrid_data: SubgridData,
 }
 
-impl NtupleGrid {
+impl NtupleSubgrid {
     /// Constructor.
-    pub fn new(lumi: Lumi, subgrids: Vec<Subgrid>, bin_limits: Vec<f64>) -> NtupleGrid {
-        NtupleGrid {
-            ntuples: vec![vec![vec![vec![]; lumi.len()]; bin_limits.len() - 1]; subgrids.len()],
-            lumi,
-            subgrids,
-            bin_limits: BinLimits::new(bin_limits),
+    pub fn new(lumi_len: usize, bins: usize, subgrid_data: SubgridData) -> Self {
+        assert!(lumi_len > 0);
+        assert!(bins > 0);
+
+        Self {
+            ntuples: vec![vec![vec![]; lumi_len]; bins],
+            subgrid_data,
         }
     }
 }
 
-impl Grid for NtupleGrid {
-    fn fill(&mut self, x1: f64, x2: f64, q2: f64, obs: f64, weights: &[f64], subgrid: usize) {
-        assert!(subgrid < self.subgrids.len());
-        assert!(weights.len() == self.lumi.len());
+impl Subgrid for NtupleSubgrid {
+    fn fill(&mut self, x1: f64, x2: f64, q2: f64, obs_index: usize, weights: &[f64]) {
+        assert!(weights.len() == self.ntuples[0].len());
 
-        if let Some(obs_index) = self.bin_limits.index(obs) {
-            for (lumi_index, &weight) in weights.iter().enumerate() {
-                self.ntuples[subgrid][obs_index][lumi_index].push(Ntuple::new(x1, x2, q2, weight));
-            }
+        for (lumi_index, &weight) in weights.iter().enumerate() {
+            self.ntuples[obs_index][lumi_index].push(Ntuple::new(x1, x2, q2, weight));
         }
     }
 }
 
 #[cfg(test)]
-mod test {
+mod tests {
     use super::*;
 
-    use crate::lumi_entry;
-
     #[test]
-    fn test_fill() {
-        // luminosity function with a single entry
-        let lumi = Lumi::new(vec![lumi_entry![2, 2, 1.0]]);
-
+    fn ntuple_subgrid_fill() {
         // a single subgrid
-        let subgrids = vec![Subgrid {
+        let subgrid_data = SubgridData {
             alphas: 0,
             alpha: 2,
             logxir: 0,
             logxif: 0,
-        }];
-        // four bins
-        let bin_limits = vec![0.0, 0.25, 0.5, 0.75, 1.0];
+        };
 
-        let mut grid = NtupleGrid::new(lumi, subgrids, bin_limits);
+        let mut grid = NtupleSubgrid::new(1, 4, subgrid_data);
 
         // check first bin
-        grid.fill(0.5, 0.75, 1.0, 0.125, &[2.0], 0);
-        assert_eq!(grid.ntuples[0][0][0][0], Ntuple::new(0.5, 0.75, 1.0, 2.0));
+        grid.fill(0.5, 0.75, 1.0, 0, &[2.0]);
+        assert_eq!(grid.ntuples[0][0][0], Ntuple::new(0.5, 0.75, 1.0, 2.0));
 
         // check second bin
-        grid.fill(0.75, 0.5, 2.0, 0.375, &[3.0], 0);
-        assert_eq!(grid.ntuples[0][0][0][0], Ntuple::new(0.5, 0.75, 1.0, 2.0));
-        assert_eq!(grid.ntuples[0][1][0][0], Ntuple::new(0.75, 0.5, 2.0, 3.0));
+        grid.fill(0.75, 0.5, 2.0, 1, &[3.0]);
+        assert_eq!(grid.ntuples[0][0][0], Ntuple::new(0.5, 0.75, 1.0, 2.0));
+        assert_eq!(grid.ntuples[1][0][0], Ntuple::new(0.75, 0.5, 2.0, 3.0));
 
         // check third bin
-        grid.fill(0.125, 0.25, 3.0, 0.625, &[4.0], 0);
-        assert_eq!(grid.ntuples[0][0][0][0], Ntuple::new(0.5, 0.75, 1.0, 2.0));
-        assert_eq!(grid.ntuples[0][1][0][0], Ntuple::new(0.75, 0.5, 2.0, 3.0));
-        assert_eq!(grid.ntuples[0][2][0][0], Ntuple::new(0.125, 0.25, 3.0, 4.0));
+        grid.fill(0.125, 0.25, 3.0, 2, &[4.0]);
+        assert_eq!(grid.ntuples[0][0][0], Ntuple::new(0.5, 0.75, 1.0, 2.0));
+        assert_eq!(grid.ntuples[1][0][0], Ntuple::new(0.75, 0.5, 2.0, 3.0));
+        assert_eq!(grid.ntuples[2][0][0], Ntuple::new(0.125, 0.25, 3.0, 4.0));
 
         // check fourth bin
-        grid.fill(0.5, 0.5, 4.0, 0.875, &[5.0], 0);
-        assert_eq!(grid.ntuples[0][0][0][0], Ntuple::new(0.5, 0.75, 1.0, 2.0));
-        assert_eq!(grid.ntuples[0][1][0][0], Ntuple::new(0.75, 0.5, 2.0, 3.0));
-        assert_eq!(grid.ntuples[0][2][0][0], Ntuple::new(0.125, 0.25, 3.0, 4.0));
-        assert_eq!(grid.ntuples[0][3][0][0], Ntuple::new(0.5, 0.5, 4.0, 5.0));
-
-        // check overflow
-        grid.fill(0.5, 0.5, 4.0, 2.0, &[99.0], 0);
-        assert_eq!(grid.ntuples[0][0][0][0], Ntuple::new(0.5, 0.75, 1.0, 2.0));
-        assert_eq!(grid.ntuples[0][1][0][0], Ntuple::new(0.75, 0.5, 2.0, 3.0));
-        assert_eq!(grid.ntuples[0][2][0][0], Ntuple::new(0.125, 0.25, 3.0, 4.0));
-        assert_eq!(grid.ntuples[0][3][0][0], Ntuple::new(0.5, 0.5, 4.0, 5.0));
-
-        // check underflow
-        grid.fill(0.5, 0.5, 4.0, -1.0, &[99.0], 0);
-        assert_eq!(grid.ntuples[0][0][0][0], Ntuple::new(0.5, 0.75, 1.0, 2.0));
-        assert_eq!(grid.ntuples[0][1][0][0], Ntuple::new(0.75, 0.5, 2.0, 3.0));
-        assert_eq!(grid.ntuples[0][2][0][0], Ntuple::new(0.125, 0.25, 3.0, 4.0));
-        assert_eq!(grid.ntuples[0][3][0][0], Ntuple::new(0.5, 0.5, 4.0, 5.0));
+        grid.fill(0.5, 0.5, 4.0, 3, &[5.0]);
+        assert_eq!(grid.ntuples[0][0][0], Ntuple::new(0.5, 0.75, 1.0, 2.0));
+        assert_eq!(grid.ntuples[1][0][0], Ntuple::new(0.75, 0.5, 2.0, 3.0));
+        assert_eq!(grid.ntuples[2][0][0], Ntuple::new(0.125, 0.25, 3.0, 4.0));
+        assert_eq!(grid.ntuples[3][0][0], Ntuple::new(0.5, 0.5, 4.0, 5.0));
     }
 }
