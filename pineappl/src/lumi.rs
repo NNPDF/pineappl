9bac11496ceb422646bf5d182bb88ce0cda130e1
Christopher Schwan <handgranaten-herbert@posteo.de>
2020-05-05 16:41:53 +0200

    Sort the tuples in `LumiEntry`

--- pineappl/src/lumi.rs
+++ pineappl/src/lumi.rs
@@ -1,5 +1,6 @@
 //! Module for everything related to luminosity functions.
 
+use noisy_float::types::R64;
 use serde::{Deserialize, Serialize};
 
 /// This structure represens an entry of a luminosity function. Each entry consists of a tuple,
@@ -15,8 +16,12 @@ impl LumiEntry {
     /// Constructor for `LumiEntry`. Note that `entry` must be non-empty, otherwise this function
     /// panics.
     #[must_use]
-    pub fn new(entry: Vec<(i32, i32, f64)>) -> Self {
+    pub fn new(mut entry: Vec<(i32, i32, f64)>) -> Self {
         assert!(!entry.is_empty());
+
+        // sort `entry` because the ordering doesn't matter and because it makes it easier to
+        // compare `LumiEntry` objects with each other
+        entry.sort_by(|x, y| (x.0, x.1, R64::new(x.2)).cmp(&(y.0, y.1, R64::new(y.2))));
 
         Self { entry }
     }
@@ -43,4 +48,16 @@ macro_rules! lumi_entry {
     ($a:expr, $b:expr, $factor:expr $(; $c:expr, $d:expr, $fac:expr)*) => {
         $crate::lumi::LumiEntry::new(vec![($a, $b, $factor), $(($c, $d, $fac)),*])
     };
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn lumi_entry() {
+        let entry1 = lumi_entry![2, 2, 1.0; 4, 4, 1.0];
+        let entry2 = lumi_entry![4, 4, 1.0; 2, 2, 1.0];
+
+        // checks that the ordering doesn't matter
+        assert_eq!(entry1, entry2);
+    }
 }
