NNPDF/pineappl

commit 18a93bcc49079941bc12662823827369394e62e4

Fix a few clippy warnings

pineappl/src/convert.rs
@@ -1,0 +1,9 @@
+use std::convert::TryFrom;
+
+pub(crate) fn usize_from_f64(x: f64) -> usize {
+    x.min(0.0) as usize
+}
+
+pub(crate) fn f64_from_usize(x: usize) -> f64 {
+    f64::from(u32::try_from(x).unwrap())
+}

pineappl/src/bin.rs
@@ -1,5 +1,6 @@
 //! Module that contains helpers for binning observables
 
+use super::convert::{f64_from_usize, usize_from_f64};
 use serde::{Deserialize, Serialize};
 use std::f64;
 use thiserror::Error;
@@ -7,7 +8,7 @@ use thiserror::Error;
 fn float_eq_within(lhs: f64, rhs: f64, ulps: usize) -> bool {
     // TODO: only works well enough if the numbers are far enough from zero or exacly zero
     if (lhs != 0.0) && (rhs != 0.0) {
-        (lhs / rhs).abs().max((rhs / lhs).abs()) < f64::EPSILON.mul_add(ulps as f64, 1.0)
+        (lhs / rhs).abs().max((rhs / lhs).abs()) < f64::EPSILON.mul_add(f64_from_usize(ulps), 1.0)
     } else {
         lhs == rhs
     }
@@ -81,7 +82,9 @@ impl BinLimits {
                 if value < *left || value >= *right {
                     None
                 } else {
-                    Some(((value - left) / (right - left) * (*bins as f64)) as usize)
+                    Some(usize_from_f64(
+                        (value - left) / (right - left) * f64_from_usize(*bins),
+                    ))
                 }
             }
             Limits::Unequal { limits } => {
@@ -126,7 +129,7 @@ impl BinLimits {
     pub fn limits(&self) -> Vec<f64> {
         match &self.0 {
             Limits::Equal { left, right, bins } => (0..=*bins)
-                .map(|b| (*right - *left).mul_add((b as f64) / (*bins as f64), *left))
+                .map(|b| (*right - *left).mul_add(f64_from_usize(b) / f64_from_usize(*bins), *left))
                 .collect(),
             Limits::Unequal { limits } => limits.clone(),
         }
@@ -150,7 +153,9 @@ impl BinLimits {
     #[must_use]
     pub fn bin_sizes(&self) -> Vec<f64> {
         match &self.0 {
-            Limits::Equal { left, right, bins } => vec![(*right - *left) / (*bins as f64); *bins],
+            Limits::Equal { left, right, bins } => {
+                vec![(*right - *left) / f64_from_usize(*bins); *bins]
+            }
             Limits::Unequal { limits } => limits.windows(2).map(|x| x[1] - x[0]).collect(),
         }
     }

pineappl/src/grid.rs
@@ -11,9 +11,12 @@ use ndarray::{Array3, Dimension};
 use serde::{Deserialize, Serialize};
 use std::any::Any;
 use std::cmp::Ordering;
+use std::convert::TryInto;
 use std::io::{Read, Seek, SeekFrom, Write};
 use std::mem;
 use thiserror::Error;
+
+// TODO: when possible change the types from `u32` to `u8` to change `try_into` to `into`
 
 /// Coupling powers for each grid.
 #[derive(Clone, Deserialize, Eq, PartialEq, Serialize)]
@@ -369,16 +372,16 @@ impl Grid {
                         lumi += xfx1(entry.0, x1, q2) * xfx2(entry.1, x2, q2) * entry.2 / (x1 * x2);
                     }
 
-                    lumi *= alphas(q2).powi(order.alphas as i32);
+                    lumi *= alphas(q2).powi(order.alphas.try_into().unwrap());
                     lumi
                 });
 
                 if order.logxir > 0 {
-                    value *= xir.ln().powi(order.logxir as i32);
+                    value *= xir.ln().powi(order.logxir.try_into().unwrap());
                 }
 
                 if order.logxif > 0 {
-                    value *= xif.ln().powi(order.logxif as i32);
+                    value *= xif.ln().powi(order.logxif.try_into().unwrap());
                 }
 
                 bins[l + xi.len() * bin_index] += value / bin_sizes[j];
@@ -443,16 +446,16 @@ impl Grid {
                                 xfx1(entry.0, x1, q2) * xfx2(entry.1, x2, q2) * entry.2 / (x1 * x2);
                         }
 
-                        lumi *= alphas(q2).powi(order.alphas as i32);
+                        lumi *= alphas(q2).powi(order.alphas.try_into().unwrap());
                         lumi
                     });
 
                     if order.logxir > 0 {
-                        value *= xir.ln().powi(order.logxir as i32);
+                        value *= xir.ln().powi(order.logxir.try_into().unwrap());
                     }
 
                     if order.logxif > 0 {
-                        value *= xif.ln().powi(order.logxif as i32);
+                        value *= xif.ln().powi(order.logxif.try_into().unwrap());
                     }
 
                     bins[l + xi.len() * bin_index][index] += value / bin_sizes[j];
@@ -661,10 +664,10 @@ impl Grid {
         for ((i, _, _), subgrid) in self.subgrids.indexed_iter_mut() {
             let order = &self.orders[i];
             let factor = global
-                * alphas.powi(order.alphas as i32)
-                * alpha.powi(order.alpha as i32)
-                * logxir.powi(order.logxir as i32)
-                * logxif.powi(order.logxif as i32);
+                * alphas.powi(order.alphas.try_into().unwrap())
+                * alpha.powi(order.alpha.try_into().unwrap())
+                * logxir.powi(order.logxir.try_into().unwrap())
+                * logxif.powi(order.logxif.try_into().unwrap());
 
             subgrid.scale(factor);
         }

pineappl/src/lagrange_subgrid.rs
@@ -1,5 +1,6 @@
 //! Module containing the Lagrange-interpolation subgrid.
 
+use super::convert::{f64_from_usize, usize_from_f64};
 use super::grid::{Ntuple, Subgrid, SubgridParams};
 use arrayvec::ArrayVec;
 use itertools::iproduct;
@@ -47,14 +48,14 @@ fn fi(i: usize, n: usize, u: f64) -> f64 {
     let mut factorials = 1;
     let mut product = if (n - i) % 2 == 0 { 1.0 } else { -1.0 };
     for z in 0..i {
-        product *= u - (z as f64);
+        product *= u - f64_from_usize(z);
         factorials *= z + 1;
     }
     for z in i + 1..=n {
-        product *= u - (z as f64);
+        product *= u - f64_from_usize(z);
         factorials *= z - i;
     }
-    product / (factorials as f64)
+    product / f64_from_usize(factorials)
 }
 
 /// Subgrid which uses Lagrange-interpolation.
@@ -95,19 +96,19 @@ impl LagrangeSubgridV1 {
     }
 
     fn deltay(&self) -> f64 {
-        (self.ymax - self.ymin) / ((self.ny - 1) as f64)
+        (self.ymax - self.ymin) / f64_from_usize(self.ny - 1)
     }
 
     fn deltatau(&self) -> f64 {
-        (self.taumax - self.taumin) / ((self.ntau - 1) as f64)
+        (self.taumax - self.taumin) / f64_from_usize(self.ntau - 1)
     }
 
     fn gety(&self, iy: usize) -> f64 {
-        (iy as f64).mul_add(self.deltay(), self.ymin)
+        f64_from_usize(iy).mul_add(self.deltay(), self.ymin)
     }
 
     fn gettau(&self, iy: usize) -> f64 {
-        (iy as f64).mul_add(self.deltatau(), self.taumin)
+        f64_from_usize(iy).mul_add(self.deltatau(), self.taumin)
     }
 }
 
@@ -175,12 +176,9 @@ impl Subgrid for LagrangeSubgridV1 {
             return;
         }
 
-        let k1 = (((y1 - self.ymin) / self.deltay() - ((self.yorder / 2) as f64)).max(0.0)
-            as usize)
+        let k1 = usize_from_f64((y1 - self.ymin) / self.deltay() - f64_from_usize(self.yorder / 2))
             .min(self.ny - 1 - self.yorder);
-
-        let k2 = (((y2 - self.ymin) / self.deltay() - ((self.yorder / 2) as f64)).max(0.0)
-            as usize)
+        let k2 = usize_from_f64((y2 - self.ymin) / self.deltay() - f64_from_usize(self.yorder / 2))
             .min(self.ny - 1 - self.yorder);
 
         let u_y1 = (y1 - self.gety(k1)) / self.deltay();
@@ -193,9 +191,10 @@ impl Subgrid for LagrangeSubgridV1 {
             .map(|i| fi(i, self.yorder, u_y2))
             .collect();
 
-        let k3 = (((tau - self.taumin) / self.deltatau() - ((self.tauorder / 2) as f64)).max(0.0)
-            as usize)
-            .min(self.ntau - 1 - self.tauorder);
+        let k3 = usize_from_f64(
+            (tau - self.taumin) / self.deltatau() - f64_from_usize(self.tauorder / 2),
+        )
+        .min(self.ntau - 1 - self.tauorder);
 
         let u_tau = (tau - self.gettau(k3)) / self.deltatau();
 

pineappl/src/lib.rs
@@ -1,7 +1,10 @@
 #![warn(clippy::all, clippy::cargo, clippy::nursery, clippy::pedantic)]
 #![warn(missing_docs)]
+#![allow(clippy::module_name_repetitions)]
 
 //! `PineAPPL` is not an extension of `APPLgrid`.
+
+mod convert;
 
 pub mod bin;
 pub mod grid;

commit f81cd557c06262b91cb2543a4cca58958b6758a2

Fix bug introduced with commit 18a93bc

pineappl/src/convert.rs
@@ -1,7 +1,7 @@
 use std::convert::TryFrom;
 
 pub(crate) fn usize_from_f64(x: f64) -> usize {
-    x.min(0.0) as usize
+    x.max(0.0) as usize
 }
 
 pub(crate) fn f64_from_usize(x: usize) -> f64 {