2ee9f18e9813f54a637f9a201ff6618e83870afd
Christopher Schwan <handgranaten-herbert@posteo.de>
2020-11-30 20:13:50 +0100

    Update example program

--- pineappl/examples/import-yadism-yaml.rs
+++ pineappl/examples/import-yadism-yaml.rs
@@ -1,6 +1,8 @@
+use lhapdf::Pdf;
 use pineappl::grid::{Grid, Order};
+use pineappl::lagrange_subgrid::LagrangeSubgridV2;
 use pineappl::lumi_entry;
-use pineappl::subgrid::SubgridParams;
+use pineappl::subgrid::{ExtraSubgridParams, Subgrid, SubgridParams};
 use std::convert::TryFrom;
 use std::env;
 use std::fs;
@@ -33,10 +35,9 @@ fn main() {
         .map(|x| x.as_f64().unwrap())
         .collect::<Vec<_>>();
     let pids = doc["pids"].as_vec().unwrap();
-    //let xif = doc["xiF"].as_f64().unwrap();
+    let xif = doc["xiF"].as_f64().unwrap();
 
     assert_eq!(interpolation_is_log, false);
-    //assert_eq!(xif, 1.0);
 
     let lepton_pid = 11;
     let lumi: Vec<_> = pids
@@ -50,28 +51,91 @@ fn main() {
         })
         .collect();
     // TODO: right now there's only a leading order
-    let orders = vec![Order::new(0, 2, 0, 0)];
+    let orders = vec![Order::new(0, 0, 0, 0)];
     let bins = doc["F2total"].as_vec().unwrap().iter().count();
     let bin_limits: Vec<_> = (0..=bins).map(|limit| limit as f64).collect();
     let mut params = SubgridParams::default();
 
-    //params.set_q2_bins();
-    //params.set_q2_max();
-    //params.set_q2_min();
-    //params.set_q2_order();
     params.set_reweight(false);
     params.set_x_bins(interpolation_xgrid.len());
     params.set_x_max(*interpolation_xgrid.last().unwrap());
     params.set_x_min(*interpolation_xgrid.first().unwrap());
     params.set_x_order(interpolation_polynomial_degree);
 
+    let mut extra = ExtraSubgridParams::default();
+
+    extra.set_reweight2(false);
+    extra.set_x2_bins(1);
+    extra.set_x2_max(1.0);
+    extra.set_x2_min(1.0);
+    extra.set_x2_order(0);
+
     // TODO: check that the x-grid points are the really the same generated from the f2 function
 
-    let _ = Grid::new(lumi, orders, bin_limits, params);
+    let mut grid = Grid::new(lumi, orders, bin_limits, SubgridParams::default());
 
     // TODO: loop over observables and create subgrids
+    for (bin, obs) in doc["F2total"].as_vec().unwrap().iter().enumerate() {
+        let q2 = obs["Q2"].as_f64().unwrap();
+
+        // no interpolation in the factorization scale
+        params.set_q2_bins(1);
+        params.set_q2_max(q2);
+        params.set_q2_min(q2);
+        params.set_q2_order(0);
+
+        // TODO: implement functionality in LagrangeSubgridV2 to handle q2_min == q2_max
+
+        let order = 0;
+
+        for (lumi, values) in obs["values"].as_vec().unwrap().iter().enumerate() {
+            if lumi != 9 {
+                continue;
+            }
+
+            let values: Vec<_> = values
+                .as_vec()
+                .unwrap()
+                .iter()
+                .map(|v| v.as_f64().unwrap())
+                // reverse the values, as they are sorted differently in PineAPPL
+                .rev()
+                .collect();
+
+            assert_eq!(values.len(), params.x_bins());
+
+            if values.iter().any(|v| *v != 0.0) {
+                let mut subgrid = LagrangeSubgridV2::new(&params, &extra);
+                subgrid.write_q2_slice(0, &values);
+                grid.set_subgrid(order, bin, lumi, subgrid.into());
+            }
+        }
+    }
 
     // TODO: write a BinRemapper to set the correct values of the observables
+
+    // suppress LHAPDF banners
+    lhapdf::set_verbosity(0);
+    let pdf_set = "CT14llo_NF6";
+
+    assert!(lhapdf::available_pdf_sets().iter().any(|x| x == &pdf_set));
+
+    let pdf = Pdf::with_setname_and_member(&pdf_set, 0);
+    // PDF of the proton
+    let xfx1 = |id, x, q2| {
+        let xfx = pdf.xfx_q2(id, x, q2);
+        println!("{} {} {} = {}", id, x, q2, xfx);
+        xfx
+    };
+    // 'PDF' of the electron
+    let xfx2 = |_, x, _| x;
+    let alphas = |q2| pdf.alphas_q2(q2);
+
+    let results = grid.convolute(&xfx1, &xfx2, &alphas, &[], &[], &[], &[(1.0, xif)]);
+
+    for (bin, result) in results.iter().enumerate() {
+        println!("{} {}", bin, result);
+    }
 
     // TODO: write the grid to `output`
 
