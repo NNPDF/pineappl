84e5f6c6a136a84750944cd822451516c269ba44
Christopher Schwan <handgranaten-herbert@posteo.de>
2020-12-18 09:36:54 +0100

    Make sure a proton PDF is used

--- pineappl_cli/src/helpers.rs
+++ pineappl_cli/src/helpers.rs
@@ -28,17 +28,20 @@ pub fn convolute(
     let initial_state_2 = grid.key_values().map_or(2212, |map| {
         map.get("initial_state_2").unwrap().parse::<i32>().unwrap()
     });
-    // TODO: make sure this is a proton PDF
-    let lhapdf_pdf = |id, x, q2| pdf.xfx_q2(id, x, q2);
+
+    pdf_pdg_id = pdf.set().entry("Particle").unwrap().parse::<i32>().unwrap();
+
+    let pdf = |id, x, q2| pdf.xfx_q2(id, x, q2);
     let no_pdf = |_, x, _| x;
-    let xfx1: Box<dyn Fn(i32, f64, f64) -> f64> = match initial_state_1 {
-        2212 => Box::new(&lhapdf_pdf),
+
+    let xfx1: Box<dyn Fn(i32, f64, f64) -> f64> = if match initial_state_1 {
+        2212 => Box::new(&proton_pdf),
         // TODO: handle anti-proton PDF
         11 | 13 | -11 | -13 => Box::new(&no_pdf),
         _ => unimplemented!(),
     };
     let xfx2: Box<dyn Fn(i32, f64, f64) -> f64> = match initial_state_2 {
-        2212 => Box::new(&lhapdf_pdf),
+        2212 => Box::new(&proton_pdf),
         // TODO: handle anti-proton PDF
         11 | 13 | -11 | -13 => Box::new(&no_pdf),
         _ => unimplemented!(),
