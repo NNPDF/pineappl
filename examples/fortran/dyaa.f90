bd77b0e94b6ff1f455328221182b02553c83db4e
Christopher Schwan <handgranaten-herbert@posteo.de>
2021-03-01 17:22:16 +0100

    Add fortran example

---
+++ examples/fortran/dyaa.f90
@@ -0,0 +1,145 @@
+program dyaa
+    use pineappl
+
+    implicit none
+
+    integer, parameter :: dp = kind(0.0d0)
+
+    type psp2to2
+        real (dp) s, t, u, x1, x2, jacobian
+    end type
+
+    type (pineappl_lumi)    :: lumi
+    integer, dimension(2)   :: pdg_ids
+    real (dp), dimension(1) :: ckm_factors
+
+    type (pineappl_grid)     :: grid
+    integer, dimension(4)    :: orders
+    real (dp), dimension(25) :: bins
+
+    type (pineappl_keyval) :: key_vals
+
+    integer :: i
+
+    integer              :: order_idx, lumi_idx, calls
+    real (dp), parameter :: hbarc2 = 389379372.1_dp
+    real (dp)            :: x1, x2, q2, weight, s, t, u, jacobian, ptl, mll, yll, ylp, ylm
+    type (psp2to2)       :: tmp
+
+    ! create a new luminosity function for the $\gamma\gamma$ initial state
+    lumi = pineappl_lumi_new()
+    pdg_ids = [ 22, 22 ]
+    ckm_factors = [ 1.0_dp ]
+    call pineappl_lumi_add(lumi, 1, pdg_ids, ckm_factors)
+
+    ! only LO $\alpha_\mathrm{s}^0 \alpha^2 \log^0(\xi_\mathrm{R}) \log^0(\xi_\mathrm{F})$
+    orders = [ 0, 2, 0, 0 ]
+    ! we bin in rapidity from 0 to 2.4 in steps of 0.1
+    bins = [ (i * 0.1_dp, i = 0, 24) ]
+
+    ! create the PineAPPL grid with default interpolation and binning parameters
+    key_vals = pineappl_keyval_new()
+    grid = pineappl_grid_new(lumi, 1, orders, 24, bins, key_vals)
+
+    ! number of phase-space points that are generated before cuts
+    calls = 10000000
+
+    ! fill the grid with phase-space points
+    do i = 1, calls
+        tmp = hadronic_pspgen(10.0_dp, 7000.0_dp)
+        s = tmp%s
+        t = tmp%t
+        u = tmp%u
+        x1 = tmp%x1
+        x2 = tmp%x2
+        jacobian = tmp%jacobian
+
+        ptl = sqrt((t * u / s))
+        mll = sqrt(s)
+        yll = 0.5_dp * log(x1 / x2)
+        ylp = abs(yll + acosh(0.5_dp * mll / ptl))
+        ylm = abs(yll - acosh(0.5_dp * mll / ptl))
+
+        jacobian = jacobian * hbarc2 / calls
+
+        ! cuts for LO for the invariant-mass slice containing the Z-peak from CMSDY2D11
+        if ((ptl < 14.0_dp) .or. (abs(yll) > 2.4_dp) .or. (ylp > 2.4_dp) .or. (ylm > 2.4_dp) .or. &
+            (mll < 60.0_dp) .or. (mll > 120.0_dp)) then
+            cycle
+        end if
+
+        weight = jacobian * int_photo(s, t, u)
+        ! renormalisation and factorisation scale
+        q2 = 90.0_dp**2
+        order_idx = 0
+        lumi_idx = 0
+
+        ! fill the grid with a single event
+        call pineappl_grid_fill(grid, x1, x2, q2, order_idx, abs(yll), lumi_idx, weight)
+    end do
+
+    ! set metadata - this isn't needed, but usually useful
+    call pineappl_grid_set_key_value(grid, 'arxiv', '1310.7291')
+    call pineappl_grid_set_key_value(grid, 'description', 'CMS double-differential Drell—Yan cross section at 7 TeV')
+    call pineappl_grid_set_key_value(grid, 'hepdata', '10.17182/hepdata.62207.v1/t8')
+    call pineappl_grid_set_key_value(grid, 'x1_label', 'yll')
+    call pineappl_grid_set_key_value(grid, 'x1_label_tex', '$y_{\ell\bar{\ell}}$')
+    ! rapidity doesn't have a unit (other observables could be GeV, TeV, ...)
+    call pineappl_grid_set_key_value(grid, 'x1_unit', '')
+    call pineappl_grid_set_key_value(grid, 'y_unit', 'pb')
+
+    ! the following are the default values
+    !call pineappl_grid_set_key_value(grid, 'initial_state_1', '2212') ! proton
+    !call pineappl_grid_set_key_value(grid, 'initial_state_2', '2212') ! proton
+
+    ! optimize the grid representation (makes the file smaller)
+    call pineappl_grid_optimize(grid)
+
+    ! write the grid to disk
+    call pineappl_grid_write(grid, 'DY-LO-AA.pineappl')
+
+contains
+    ! photon-photon initiated lepton-pair production
+    real (dp) function int_photo(s, t, u)
+        implicit none
+        real (dp), intent(in) :: s, t, u
+        real (dp), parameter :: alpha0 = 1.0_dp / 137.03599911_dp
+
+        int_photo = alpha0 * alpha0 / 2.0_dp / s * (t / u + u / t)
+    end function
+
+    ! phase-space generator for 2->2 scattering
+    type (psp2to2) function hadronic_pspgen(mmin, mmax)
+        implicit none
+
+        real (dp) :: mmin, mmax, smin, smax, r1, r2, r3, tau0, tau, y, x1, x2, s, t, u, jacobian, cos_theta
+
+        smin = mmin * mmin
+        smax = mmax * mmax
+
+        call random_number(r1)
+        call random_number(r2)
+        call random_number(r3)
+
+        tau0 = smin / smax
+        tau = tau0**r1
+        y = tau**(1.0_dp - r2)
+        x1 = y
+        x2 = tau / y
+        s = tau * smax
+
+        jacobian = tau * log(tau0) * log(tau0) * r1
+
+        ! theta integration (in the CMS)
+        cos_theta = 2.0_dp * r3 - 1.0_dp
+        jacobian = jacobian * 2.0_dp
+
+        t = -0.5_dp * s * (1.0_dp - cos_theta)
+        u = -0.5_dp * s * (1.0_dp + cos_theta)
+
+        ! phi integration
+        jacobian = jacobian * 2.0_dp * acos(-1.0_dp)
+
+        hadronic_pspgen = psp2to2(s, t, u, x1, x2, jacobian)
+    end function
+end program
---
+++ examples/fortran/pineappl.f90
@@ -0,0 +1,214 @@
+module pineappl
+    use iso_c_binding, only: c_ptr
+
+    implicit none
+
+    integer, parameter, private :: dp = kind(0.0d0)
+
+    type pineappl_grid
+        type (c_ptr) :: ptr
+    contains
+        final :: pineappl_grid_delete
+    end type
+
+    type pineappl_keyval
+        type (c_ptr) :: ptr
+    contains
+        final :: pineappl_keyval_delete
+    end type
+
+    type pineappl_lumi
+        type (c_ptr) :: ptr
+    contains
+        final :: pineappl_lumi_delete
+    end type
+
+    interface
+        subroutine lumi_add(lumi, combinations, pdg_id_pairs, factors) bind(c, name = 'pineappl_lumi_add')
+            use iso_c_binding
+            type (c_ptr), value       :: lumi
+            integer (c_size_t), value :: combinations
+            integer (c_int32_t)       :: pdg_id_pairs(*)
+            real (c_double)           :: factors(*)
+        end subroutine
+
+        subroutine lumi_delete(lumi) bind(c, name = 'pineappl_lumi_delete')
+            use iso_c_binding
+            type (c_ptr), value :: lumi
+        end subroutine
+
+        type (c_ptr) function lumi_new() bind(c, name = 'pineappl_lumi_new')
+            use iso_c_binding
+        end function
+
+        subroutine grid_delete(grid) bind(c, name = 'pineappl_grid_delete')
+            use iso_c_binding
+            type (c_ptr), value :: grid
+        end subroutine
+
+        subroutine grid_fill(grid, x1, x2, q2, order, observable, lumi, weight) bind(c, name = 'pineappl_grid_fill')
+            use iso_c_binding
+            type (c_ptr), value       :: grid
+            real (c_double), value    :: x1, x2, q2, observable, weight
+            integer (c_size_t), value :: order, lumi
+        end subroutine
+
+        type (c_ptr) function grid_new(lumi, orders, order_params, bins, bin_limits, key_vals) &
+                bind(c, name = 'pineappl_grid_new')
+            use iso_c_binding
+            type (c_ptr), value       :: lumi, key_vals
+            integer (c_size_t), value :: orders, bins
+            integer (c_int32_t)       :: order_params(*)
+            real (c_double)           :: bin_limits(*)
+        end function
+
+        subroutine grid_optimize(grid) bind(c, name = 'pineappl_grid_optimize')
+            use iso_c_binding
+            type (c_ptr), value :: grid
+        end subroutine
+
+        subroutine grid_set_key_value(grid, key, valju) bind(c, name = 'pineappl_grid_set_key_value')
+            use iso_c_binding
+            type (c_ptr), value :: grid
+            character (c_char)  :: key(*), valju(*)
+        end subroutine
+
+        subroutine grid_write(grid, filename) bind(c, name = 'pineappl_grid_write')
+            use iso_c_binding
+            type (c_ptr), value :: grid
+            character (c_char)  :: filename(*)
+        end subroutine
+
+        subroutine keyval_delete(keyval) bind(c, name = 'pineappl_keyval_delete')
+            use iso_c_binding
+            type (c_ptr), value :: keyval
+        end subroutine
+
+        type (c_ptr) function keyval_new() bind(c, name = 'pineappl_keyval_new')
+            use iso_c_binding
+        end function
+
+        subroutine keyval_set_string(keyval, key, valju) bind(c, name = 'pineappl_keyval_set_string')
+            use iso_c_binding
+            type (c_ptr), value :: keyval
+            character (c_char)  :: key(*), valju(*)
+        end subroutine
+    end interface
+
+contains
+    subroutine pineappl_lumi_add(lumi, combinations, pdg_id_pairs, factors)
+        use iso_c_binding
+
+        implicit none
+
+        type (pineappl_lumi), intent(in)                 :: lumi
+        integer, intent(in)                              :: combinations
+        integer, dimension(2 * combinations), intent(in) :: pdg_id_pairs
+        real (dp), dimension(combinations), intent(in)   :: factors
+
+        call lumi_add(lumi%ptr, int(combinations, c_size_t), pdg_id_pairs, factors)
+    end subroutine
+
+    subroutine pineappl_lumi_delete(lumi)
+        implicit none
+
+        type (pineappl_lumi), intent(in) :: lumi
+
+        call lumi_delete(lumi%ptr)
+    end subroutine
+
+    type (pineappl_lumi) function pineappl_lumi_new()
+        implicit none
+
+        pineappl_lumi_new = pineappl_lumi(lumi_new())
+    end function
+
+    subroutine pineappl_grid_delete(grid)
+        implicit none
+
+        type (pineappl_grid), intent(in) :: grid
+
+        call grid_delete(grid%ptr)
+    end subroutine
+
+    subroutine pineappl_grid_fill(grid, x1, x2, q2, order, observable, lumi, weight)
+        use iso_c_binding
+
+        implicit none
+
+        type (pineappl_grid), intent(in) :: grid
+        real (dp), intent(in)            :: x1, x2, q2, observable, weight
+        integer, intent(in)              :: order, lumi
+
+        call grid_fill(grid%ptr, x1, x2, q2, int(order, c_size_t), observable, int(lumi, c_size_t), weight)
+    end subroutine
+
+    type (pineappl_grid) function pineappl_grid_new(lumi, orders, order_params, bins, bin_limits, key_vals)
+        use iso_c_binding
+
+        implicit none
+
+        type (pineappl_lumi), intent(in)           :: lumi
+        integer, intent(in)                        :: orders, bins
+        integer, dimension(4 * orders), intent(in) :: order_params
+        real (dp), dimension(bins + 1), intent(in) :: bin_limits
+        type (pineappl_keyval), intent(in)         :: key_vals
+
+        pineappl_grid_new = pineappl_grid(grid_new(lumi%ptr, int(orders, c_size_t), &
+            order_params, int(bins, c_size_t), bin_limits, key_vals%ptr))
+    end function
+
+    subroutine pineappl_grid_optimize(grid)
+        implicit none
+
+        type (pineappl_grid), intent(in) :: grid
+
+        call grid_optimize(grid%ptr)
+    end subroutine
+
+    subroutine pineappl_grid_set_key_value(grid, key, valju)
+        use iso_c_binding
+
+        implicit none
+
+        type (pineappl_grid), intent(in) :: grid
+        character (*), intent(in)        :: key, valju
+
+        call grid_set_key_value(grid%ptr, key // c_null_char, valju // c_null_char)
+    end subroutine
+
+    subroutine pineappl_grid_write(grid, filename)
+        use iso_c_binding
+
+        implicit none
+
+        type (pineappl_grid), intent(in) :: grid
+        character (*), intent(in)        :: filename
+
+        call grid_write(grid%ptr, filename // c_null_char)
+    end subroutine
+
+    subroutine pineappl_keyval_delete(keyval)
+        implicit none
+
+        type (pineappl_keyval), intent(in) :: keyval
+
+        call keyval_delete(keyval%ptr)
+    end subroutine
+
+    type (pineappl_keyval) function pineappl_keyval_new()
+        implicit none
+
+        pineappl_keyval_new = pineappl_keyval(keyval_new())
+    end function
+
+    subroutine pineappl_keyval_set_string(keyval, key, valju)
+        use iso_c_binding
+
+        implicit none
+        type (pineappl_keyval), intent(in) :: keyval
+        character (*), intent(in)          :: key, valju
+
+        call keyval_set_string(keyval%ptr, key // c_null_char, valju // c_null_char)
+    end subroutine
+end module
