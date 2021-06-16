efd5671e1eb3a94dca0e489baa2836f185fd7002
Christopher Schwan <handgranaten-herbert@posteo.de>
2021-06-16 12:08:38 +0200

    Use `Table` instead of `println` for `subgrids`

--- pineappl_cli/src/subgrids.rs
+++ pineappl_cli/src/subgrids.rs
@@ -1,18 +1,23 @@
 use super::helpers;
 use anyhow::Result;
 use pineappl::subgrid::SubgridEnum;
+use prettytable::{cell, row, Table};
 
-pub fn subcommand(input: &str) -> Result<()> {
+pub fn subcommand(input: &str) -> Result<Table> {
     let grid = helpers::read_grid(input)?;
+    let mut table = helpers::create_table();
+
+    let titles = row![c => "order", "bin", "lumi", "type"];
+    table.set_titles(titles);
 
     for order in 0..grid.orders().len() {
         for bin in 0..grid.bin_info().bins() {
             for lumi in 0..grid.lumi().len() {
-                println!(
-                    "({}, {}, {}) = {}",
-                    order,
-                    bin,
-                    lumi,
+                let row = table.add_empty_row();
+                row.add_cell(cell!(l->&format!("{}", order)));
+                row.add_cell(cell!(l->&format!("{}", bin)));
+                row.add_cell(cell!(l->&format!("{}", lumi)));
+                row.add_cell(cell!(l->
                     match grid.subgrid(order, bin, lumi) {
                         SubgridEnum::LagrangeSubgridV1(_) => "LagrangeSubgridV1",
                         SubgridEnum::NtupleSubgridV1(_) => "NtupleSubgridV1",
@@ -21,10 +26,10 @@ pub fn subcommand(input: &str) -> Result<()> {
                         SubgridEnum::ImportOnlySubgridV1(_) => "ImportOnlySubgridV1",
                         SubgridEnum::EmptySubgridV1(_) => "EmptySubgridV1",
                     }
-                );
+                ));
             }
         }
     }
 
-    Ok(())
+    Ok(table)
 }
