NNPDF/pineappl

commit c515927834c0f83a5242346c58308012fa4efd89

Fix tiny inconsistency
Instead of using the zeroth PDF member for the central prediction, use
the central prediction calculated from the entire PDF set

pineappl_cli/src/pull.rs
@@ -85,14 +85,22 @@ pub fn subcommand(
             .map(|lumi| {
                 let mut lumi_mask = vec![false; grid.lumi().len()];
                 lumi_mask[lumi] = true;
-                helpers::convolute(&grid, &pdfset1[0], &[], &[bin], &lumi_mask, 1)[0]
+                let central: Vec<f64> = pdfset1
+                    .par_iter()
+                    .flat_map(|pdf| helpers::convolute(&grid, &pdf, &[], &[], &lumi_mask, 1))
+                    .collect();
+                set1.uncertainty(&central, cl, false).central
             })
             .collect();
         let lumi_results2: Vec<_> = (0..grid.lumi().len())
             .map(|lumi| {
                 let mut lumi_mask = vec![false; grid.lumi().len()];
                 lumi_mask[lumi] = true;
-                helpers::convolute(&grid, &pdfset2[0], &[], &[bin], &lumi_mask, 1)[0]
+                let central: Vec<f64> = pdfset2
+                    .par_iter()
+                    .flat_map(|pdf| helpers::convolute(&grid, &pdf, &[], &[], &lumi_mask, 1))
+                    .collect();
+                set1.uncertainty(&central, cl, false).central
             })
             .collect();
 

commit f7a07eefb7770c6f8238bd129c183a6345dd291c

Fix small bug introduced in commit c515927

pineappl_cli/src/pull.rs
@@ -87,7 +87,7 @@ pub fn subcommand(
                 lumi_mask[lumi] = true;
                 let central: Vec<f64> = pdfset1
                     .iter()
-                    .flat_map(|pdf| helpers::convolute(&grid, pdf, &[], &[], &lumi_mask, 1))
+                    .map(|pdf| helpers::convolute(&grid, pdf, &[], &[bin], &lumi_mask, 1)[0])
                     .collect();
                 set1.uncertainty(&central, cl, false).central
             })
@@ -98,7 +98,7 @@ pub fn subcommand(
                 lumi_mask[lumi] = true;
                 let central: Vec<f64> = pdfset2
                     .iter()
-                    .flat_map(|pdf| helpers::convolute(&grid, pdf, &[], &[], &lumi_mask, 1))
+                    .map(|pdf| helpers::convolute(&grid, pdf, &[], &[bin], &lumi_mask, 1)[0])
                     .collect();
                 set1.uncertainty(&central, cl, false).central
             })