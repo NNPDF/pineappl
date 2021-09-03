fbf8320f457f560fbf151b24a0bbff5365c9515a
Felix Hekhorn <felix.hekhorn@uni-tuebingen.de>
2021-09-03 12:19:53 +0200

    Add doc to grid.py

--- pineappl_py/pineappl/grid.py
+++ pineappl_py/pineappl/grid.py
@@ -5,33 +5,108 @@ except:
 
     warnings.warn("binary files missing")
 
-from .utils import PyWrapper
-
 import numpy as np
+
+from .utils import PyWrapper
 
 
 class Order(PyWrapper):
+    r"""
+    Python wrapper object to interface `Order`.
+
+    Parameters
+    ----------
+        alphas : int
+            power of :math:`\alphas_s`
+        alpha : int
+            power of :math:`\alpha`
+        logxir : int
+            power of :math:`\log(\xi_r)`
+        logxif : int
+            power of :math:`\log(\xi_f)`
+    """
+
     def __init__(self, alphas, alpha, logxir, logxif):
         self._raw = PyOrder(alphas, alpha, logxir, logxif)
 
 
 class Grid(PyWrapper):
+    r"""
+    Python wrapper object to interface `Grid`.
+
+    To create an object, you should call either :meth:`create`
+    or :meth:`read`.
+
+    Parameters
+    ----------
+        pygrid : PyGrid
+            raw wrapper object
+    """
+
     def __init__(self, pygrid):
         self._raw = pygrid
 
     @classmethod
     def create(cls, lumi, orders, bin_limits, subgrid_params):
+        """
+        Create a grid object from its ingredients
+
+        Parameters
+        ---------
+            lumi : list(LumiEntry)
+                List of active luminosities
+            orders: list(Order)
+                List of available orders
+            bin_limits: BinRemapper
+                bins
+            subgrid_params : SubgridParams
+                subgrid parameters
+        """
         lumi = [l.raw for l in lumi]
         orders = [o.raw for o in orders]
         return cls(PyGrid(lumi, orders, bin_limits, subgrid_params.raw))
 
     def set_subgrid(self, order, bin_, lumi, subgrid):
+        """
+        Set the subgrid at the given position.
+
+        Parameters
+        ----------
+            order : int
+                index of order
+            bin_ : int
+                index of bin
+            lumi : int
+                index of luminosity
+            subgrid : ImportOnlySubgridV1
+                subgrid content
+        """
         self.raw.set_subgrid(order, bin_, lumi, subgrid.into())
 
     def set_remapper(self, remapper):
+        """
+        Set the normalizations.
+
+        Parameter:
+            remapper: BinRemapper
+                Remapper object
+        """
         self.raw.set_remapper(remapper.raw)
 
     def convolute_eko(self, operators):
+        """
+        Create an FKTable with the EKO.
+
+        Parameters
+        ----------
+            operators : dict
+                EKO Output
+
+        Returns
+        ------
+            PyGrid
+                raw Grid as an FKTable
+        """
         operator_grid = np.array(
             [op["operators"] for op in operators["Q2grid"].values()]
         )
@@ -49,4 +124,17 @@ class Grid(PyWrapper):
 
     @classmethod
     def read(cls, path):
+        """
+        Load an existing grid from file.
+
+        Parameters
+        ----------
+            path : str
+                file path
+
+        Returns
+        -------
+            Grid
+                grid object
+        """
         return cls(PyGrid.read(path))
