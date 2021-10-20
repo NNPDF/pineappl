cedd3485d27f66b32dc8963b40ab212f048eb401
Christopher Schwan <handgranaten-herbert@posteo.de>
2021-10-20 11:08:07 +0200

    Use new `LumiCache` in the CLI

--- pineappl_cli/src/helpers.rs
+++ pineappl_cli/src/helpers.rs
@@ -2,6 +2,7 @@ use anyhow::{Context, Result};
 use lhapdf::Pdf;
 use ndarray::Array3;
 use pineappl::grid::Grid;
+use pineappl::lumi::LumiCache;
 use prettytable::format::{FormatBuilder, LinePosition, LineSeparator};
 use prettytable::Table;
 use std::fs::{File, OpenOptions};
@@ -90,63 +91,17 @@ pub fn convolute(
         })
         .collect();
 
-    let initial_state_1 = grid.key_values().map_or(2212, |map| {
-        map.get("initial_state_1").unwrap().parse::<i32>().unwrap()
-    });
-    let initial_state_2 = grid.key_values().map_or(2212, |map| {
-        map.get("initial_state_2").unwrap().parse::<i32>().unwrap()
-    });
-
-    // if the field 'Particle' is missing we assume it's a proton PDF
     let pdf_pdg_id = lhapdf
         .set()
         .entry("Particle")
         .unwrap_or_else(|| "2212".to_string())
         .parse::<i32>()
         .unwrap();
+    let mut pdf = |id, x, q2| lhapdf.xfx_q2(id, x, q2);
+    let mut alphas = |q2| lhapdf.alphas_q2(q2);
+    let mut cache = LumiCache::with_one(pdf_pdg_id, &mut pdf, &mut alphas);
 
-    let pdf = |id, x, q2| lhapdf.xfx_q2(id, x, q2);
-    let anti_pdf = |id, x, q2| {
-        let id = match id {
-            -6..=6 | 11 | 13 | -11 | -13 => -id,
-            21 | 22 => id,
-            _ => unimplemented!(),
-        };
-        lhapdf.xfx_q2(id, x, q2)
-    };
-    let no_pdf = |_, x, _| x;
-
-    let xfx1: Box<dyn Fn(i32, f64, f64) -> f64> = if initial_state_1 == pdf_pdg_id {
-        Box::new(&pdf)
-    } else if initial_state_1 == -pdf_pdg_id {
-        Box::new(&anti_pdf)
-    } else {
-        match initial_state_1 {
-            11 | 13 | -11 | -13 => Box::new(&no_pdf),
-            _ => unimplemented!(),
-        }
-    };
-    let xfx2: Box<dyn Fn(i32, f64, f64) -> f64> = if initial_state_2 == pdf_pdg_id {
-        Box::new(&pdf)
-    } else if initial_state_2 == -pdf_pdg_id {
-        Box::new(&anti_pdf)
-    } else {
-        match initial_state_2 {
-            11 | 13 | -11 | -13 => Box::new(&no_pdf),
-            _ => unimplemented!(),
-        }
-    };
-    let alphas = |q2| lhapdf.alphas_q2(q2);
-
-    grid.convolute(
-        &xfx1,
-        &xfx2,
-        &alphas,
-        &orders,
-        bins,
-        lumis,
-        &SCALES_VECTOR[0..scales],
-    )
+    grid.convolute2(&mut cache, &orders, bins, lumis, &SCALES_VECTOR[0..scales])
 }
 
 pub fn convolute_subgrid(
