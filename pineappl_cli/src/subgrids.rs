34be69f0ebb8f9fbde7a8942fa0da55b5b1558ca
Christopher Schwan <handgranaten-herbert@posteo.de>
2021-10-27 16:01:25 +0200

    Simplify implementation of `subgrids` subcommand

--- pineappl_cli/src/subgrids.rs
+++ pineappl_cli/src/subgrids.rs
@@ -10,26 +10,22 @@ pub fn subcommand(input: &str) -> Result<Table> {
     let titles = row![c => "order", "bin", "lumi", "type"];
     table.set_titles(titles);
 
-    for order in 0..grid.orders().len() {
-        for bin in 0..grid.bin_info().bins() {
-            for lumi in 0..grid.lumi().len() {
-                let row = table.add_empty_row();
-                row.add_cell(cell!(l->&format!("{}", order)));
-                row.add_cell(cell!(l->&format!("{}", bin)));
-                row.add_cell(cell!(l->&format!("{}", lumi)));
-                row.add_cell(cell!(l->
-                    match grid.subgrid(order, bin, lumi) {
-                        SubgridEnum::LagrangeSubgridV1(_) => "LagrangeSubgridV1",
-                        SubgridEnum::NtupleSubgridV1(_) => "NtupleSubgridV1",
-                        SubgridEnum::LagrangeSparseSubgridV1(_) => "LagrangeSparseSubgridV1",
-                        SubgridEnum::LagrangeSubgridV2(_) => "LagrangeSubgridV2",
-                        SubgridEnum::ImportOnlySubgridV1(_) => "ImportOnlySubgridV1",
-                        SubgridEnum::ImportOnlySubgridV2(_) => "ImportOnlySubgridV2",
-                        SubgridEnum::EmptySubgridV1(_) => "EmptySubgridV1",
-                    }
-                ));
+    for ((order, bin, lumi), subgrid) in grid.subgrids().indexed_iter() {
+        let row = table.add_empty_row();
+        row.add_cell(cell!(l->&format!("{}", order)));
+        row.add_cell(cell!(l->&format!("{}", bin)));
+        row.add_cell(cell!(l->&format!("{}", lumi)));
+        row.add_cell(cell!(l->
+            match subgrid {
+                SubgridEnum::LagrangeSubgridV1(_) => "LagrangeSubgridV1",
+                SubgridEnum::NtupleSubgridV1(_) => "NtupleSubgridV1",
+                SubgridEnum::LagrangeSparseSubgridV1(_) => "LagrangeSparseSubgridV1",
+                SubgridEnum::LagrangeSubgridV2(_) => "LagrangeSubgridV2",
+                SubgridEnum::ImportOnlySubgridV1(_) => "ImportOnlySubgridV1",
+                SubgridEnum::ImportOnlySubgridV2(_) => "ImportOnlySubgridV2",
+                SubgridEnum::EmptySubgridV1(_) => "EmptySubgridV1",
             }
-        }
+        ));
     }
 
     Ok(table)
