60302e11a82553f3df0ea48c5f308a8dd192ff18
Christopher Schwan <handgranaten-herbert@posteo.de>
2021-11-22 11:08:22 +0100

    Remove wrong assumption
    Whenever the subgrids have different x-grid values, the total number of
    x-grid points is not equal to number of x-grid points for a specific
    subgrid

--- pineappl/src/grid.rs
+++ pineappl/src/grid.rs
@@ -1329,7 +1329,6 @@ impl Grid {
 
         assert_eq!(dim[0], eko_info.grid_axes.muf2_grid.len());
         assert_eq!(dim[1], eko_info.target_pids.len());
-        assert_eq!(dim[2], eko_info.target_x_grid.len());
         assert_eq!(dim[3], eko_info.grid_axes.pids.len());
         assert_eq!(dim[4], eko_info.grid_axes.x_grid.len());
 
