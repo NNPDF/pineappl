9d779507a0ad34caf7a67f54d52355c6ca54a59a
Christopher Schwan <handgranaten-herbert@posteo.de>
2022-01-19 16:20:56 +0100

    Add missing negations

--- pineappl/src/grid.rs
+++ pineappl/src/grid.rs
@@ -1675,10 +1675,10 @@ impl Grid {
 
                         // if `op1` and `op2` below are zero there's no work to do
                         // TODO: ideally we change the for loops instead of vetoing here
-                        if non_zero_pid_indices
+                        if !non_zero_pid_indices
                             .iter()
                             .any(|&tuple| tuple == (tgt_pid1_idx, src_pid1_idx))
-                            || non_zero_pid_indices
+                            || !non_zero_pid_indices
                                 .iter()
                                 .any(|&tuple| tuple == (tgt_pid2_idx, src_pid2_idx))
                         {
