fb60a7e88f7bfde15679825721461107b6718922
Felix Hekhorn <felix.hekhorn@uni-tuebingen.de>
2022-05-12 12:35:47 +0200

    Partially replace unwrap_or_else with just unwrap

--- pineappl/src/grid.rs
+++ pineappl/src/grid.rs
@@ -1356,11 +1356,11 @@ impl Grid {
         }
 
         // make grids unique
-        x_grid.sort_by(|a, b| a.partial_cmp(b).unwrap_or_else(|| unreachable!()));
+        x_grid.sort_by(|a, b| a.partial_cmp(b).unwrap());
         x_grid.dedup_by(|a, b| approx_eq!(f64, *a, *b, ulps = 64));
-        mur2_grid.sort_by(|a, b| a.partial_cmp(b).unwrap_or_else(|| unreachable!()));
+        mur2_grid.sort_by(|a, b| a.partial_cmp(b).unwrap());
         mur2_grid.dedup_by(|a, b| approx_eq!(f64, *a, *b, ulps = 64));
-        muf2_grid.sort_by(|a, b| a.partial_cmp(b).unwrap_or_else(|| unreachable!()));
+        muf2_grid.sort_by(|a, b| a.partial_cmp(b).unwrap());
         muf2_grid.dedup_by(|a, b| approx_eq!(f64, *a, *b, ulps = 64));
 
         Some(GridAxes {
