05524a2f1509516aeb4a451eb6a13361292c57b4
Christopher Schwan <handgranaten-herbert@posteo.de>
2022-09-06 11:07:57 +0200

    Fix and test `--scale-by-order` parameter

--- pineappl_cli/src/merge.rs
+++ pineappl_cli/src/merge.rs
@@ -20,6 +20,7 @@ pub struct Opts {
         alias = "scale_by_order",
         conflicts_with = "scale",
         long = "scale-by-order",
+        use_value_delimiter = true,
         value_names = &["ALPHAS", "ALPHA", "LOGXIR", "LOGXIF", "GLOBAL"]
     )]
     scale_by_order: Vec<f64>,
@@ -89,6 +90,18 @@ OPTIONS:
 7    4  4.5 1.3772029e1    -3.46     2.85
 ";
 
+    const SCALE_BY_ORDER_STR: &str = "b   etal    disg/detal  scale uncertainty
+     []        [pb]            [%]       
+-+----+----+-----------+--------+--------
+0    2 2.25 2.1481594e2    -4.53     3.53
+1 2.25  2.5 1.9807977e2    -4.50     3.55
+2  2.5 2.75 1.7256275e2    -4.43     3.54
+3 2.75    3 1.3976747e2    -4.36     3.52
+4    3 3.25 1.0460103e2    -4.26     3.49
+5 3.25  3.5 7.1393138e1    -4.17     3.45
+6  3.5    4 3.3881527e1    -4.01     3.50
+7    4  4.5 8.2340900e0    -3.70     3.92
+";
     #[test]
     fn help() {
         Command::cargo_bin("pineappl")
@@ -127,5 +140,35 @@ OPTIONS:
             .assert()
             .success()
             .stdout(DEFAULT_STR);
+    }
+
+    #[test]
+    fn scale_by_order() {
+        let output = NamedTempFile::new("merged.pineappl.lz4").unwrap();
+
+        Command::cargo_bin("pineappl")
+            .unwrap()
+            .args(&[
+                "merge",
+                "--scale-by-order=2,1,0.5,0.5,0.25",
+                output.path().to_str().unwrap(),
+                "data/LHCB_WP_7TEV.pineappl.lz4",
+                "data/LHCB_WP_7TEV.pineappl.lz4",
+            ])
+            .assert()
+            .success()
+            .stdout("");
+
+        Command::cargo_bin("pineappl")
+            .unwrap()
+            .args(&[
+                "--silence-lhapdf",
+                "convolute",
+                output.path().to_str().unwrap(),
+                "NNPDF31_nlo_as_0118_luxqed",
+            ])
+            .assert()
+            .success()
+            .stdout(SCALE_BY_ORDER_STR);
     }
 }
