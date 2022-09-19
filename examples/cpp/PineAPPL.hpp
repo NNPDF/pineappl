882cae0ef379657a7e60d98d74335588403789fc
Felix Hekhorn <felix.hekhorn@uni-tuebingen.de>
2022-09-19 18:31:36 +0200

    Add more review stuff

--- examples/cpp/PineAPPL.hpp
+++ examples/cpp/PineAPPL.hpp
@@ -110,17 +110,7 @@ struct Order {
     uint32_t logxif;
 };
 
-class Grid {
-    /** @brief number of orders */
-    std::size_t n_orders = 0;
-
-    /** @brief number of bins */
-    std::size_t n_bins = 0;
-
-    /** @brief number of lumis */
-    std::size_t n_lumis = 0;
-
-public:
+struct Grid {
 
     /** @brief underlying raw object */
     pineappl_grid *raw;
@@ -129,26 +119,26 @@ public:
      * @brief constructor
      * @param lumi luminosity
      * @param orders orders
-     * @param bins bins
+     * @param bin_limits bin limits
      * @param key_val additional informations
      */
-    Grid(const Lumi& lumi, const std::vector<Order>& orders, const std::vector<double>& bins, const KeyVal& key_val) {
-        this->n_lumis = lumi.count();
+    Grid(const Lumi& lumi, const std::vector<Order>& orders, const std::vector<double>& bin_limits, const KeyVal& key_val) {
         // cast orders
+        std::size_t n_orders = 0;
         uint32_t *raw_orders = new uint32_t[4 * orders.size()];
-        for (auto it = orders.cbegin(); it != orders.cend(); ++it, ++this->n_orders){
-            raw_orders[4*this->n_orders + 0] = it->alphas;
-            raw_orders[4*this->n_orders + 1] = it->alpha;
-            raw_orders[4*this->n_orders + 2] = it->logxir;
-            raw_orders[4*this->n_orders + 3] = it->logxif;
+        for (auto it = orders.cbegin(); it != orders.cend(); ++it, ++n_orders){
+            raw_orders[4*n_orders + 0] = it->alphas;
+            raw_orders[4*n_orders + 1] = it->alpha;
+            raw_orders[4*n_orders + 2] = it->logxir;
+            raw_orders[4*n_orders + 3] = it->logxif;
         }
-        //this->n_bins = bins.size();
         // cast bins
-        double *raw_bins = new double[bins.size()];
-        for (auto it = bins.cbegin(); it != bins.cend(); ++it, ++this->n_bins)
-            raw_bins[this->n_bins] = *it;
+        std::size_t n_bins = 0;
+        double *raw_bins = new double[bin_limits.size()];
+        for (auto it = bin_limits.cbegin(); it != bin_limits.cend(); ++it, ++n_bins)
+            raw_bins[n_bins] = *it;
         // now, we got all we need
-        this->raw = pineappl_grid_new(lumi.raw, this->n_orders, raw_orders, this->n_bins - 1, raw_bins, key_val.raw);
+        this->raw = pineappl_grid_new(lumi.raw, n_orders, raw_orders, n_bins - 1, raw_bins, key_val.raw);
         // clean up
         delete[] raw_orders;
         delete[] raw_bins;
@@ -156,6 +146,22 @@ public:
 
     /** @brief destructor */
     ~Grid(){ pineappl_grid_delete(this->raw); }
+
+    /**
+     * @brief Number of orders
+     * @return number of orders
+     */
+    std::size_t order_count() const {
+        return pineappl_grid_order_count(this->raw);
+    }
+
+    /**
+     * @brief Number of bins
+     * @return number of bins
+     */
+    std::size_t bin_count() const {
+        return pineappl_grid_bin_count(this->raw);
+    }
 
     /**
      * @brief Fill grid for the given parameters
@@ -181,8 +187,10 @@ public:
      * @return prediction for each bin
      */
     std::vector<double> convolute_with_one(const std::int32_t pdg_id, LHAPDF::PDF *pdf, const double xi_ren = 1.0, const double xi_fac = 1.0 ) const {
-        std::vector<bool> order_mask(this->n_orders, true);
-        std::vector<bool> lumi_mask(this->n_lumis, true);
+        std::vector<bool> order_mask(this->order_count(), true);
+        pineappl_lumi* l = pineappl_grid_lumi(this->raw);
+        std::vector<bool> lumi_mask(pineappl_lumi_count(l), true);
+        pineappl_lumi_delete(l);
         return this->convolute_with_one(pdg_id, pdf, order_mask, lumi_mask, xi_ren, xi_fac);
     }
 
@@ -205,15 +213,18 @@ public:
             return static_cast <LHAPDF::PDF*> (pdf)->alphasQ2(q2);
         };
         // cast order_mask
-        bool *raw_order_mask = new bool[this->n_orders];
+        bool *raw_order_mask = new bool[this->order_count()];
         {std::size_t j = 0;
         for (auto it = order_mask.cbegin(); it != order_mask.cend(); ++it, ++j)
             raw_order_mask[j] = *it;}
-        bool *raw_lumi_mask = new bool[this->n_lumis];
+        pineappl_lumi* l = pineappl_grid_lumi(this->raw);
+        std::size_t n_lumis = pineappl_lumi_count(l);
+        pineappl_lumi_delete(l);
+        bool *raw_lumi_mask = new bool[n_lumis];
         {std::size_t j = 0;
         for (auto it = lumi_mask.cbegin(); it != lumi_mask.cend(); ++it, ++j)
             raw_lumi_mask[j] = *it;}
-        std::vector<double> results(this->n_bins - 1);
+        std::vector<double> results(this->bin_count());
         pineappl_grid_convolute_with_one(this->raw, pdg_id, xfx, alphas, pdf, raw_order_mask, raw_lumi_mask, xi_ren, xi_fac, results.data());
         delete[] raw_order_mask;
         delete[] raw_lumi_mask;
