a038041557307c8efc1f66ecaaa9727ee563cbdf
Christopher Schwan <handgranaten-herbert@posteo.de>
2022-09-21 15:15:35 +0200

    Run `cargo fmt`

--- pineappl_cli/tests/optimize.rs
+++ pineappl_cli/tests/optimize.rs
@@ -1,7 +1,7 @@
-    use assert_cmd::Command;
-    use assert_fs::NamedTempFile;
+use assert_cmd::Command;
+use assert_fs::NamedTempFile;
 
-    const HELP_STR: &str = "pineappl-optimize 
+const HELP_STR: &str = "pineappl-optimize 
 Optimizes the internal data structure to minimize memory usage
 
 USAGE:
@@ -17,28 +17,28 @@ OPTIONS:
     -h, --help                      Print help information
 ";
 
-    #[test]
-    fn help() {
-        Command::cargo_bin("pineappl")
-            .unwrap()
-            .args(&["optimize", "--help"])
-            .assert()
-            .success()
-            .stdout(HELP_STR);
-    }
+#[test]
+fn help() {
+    Command::cargo_bin("pineappl")
+        .unwrap()
+        .args(&["optimize", "--help"])
+        .assert()
+        .success()
+        .stdout(HELP_STR);
+}
 
-    #[test]
-    fn default() {
-        let output = NamedTempFile::new("optimized.pineappl.lz4").unwrap();
+#[test]
+fn default() {
+    let output = NamedTempFile::new("optimized.pineappl.lz4").unwrap();
 
-        Command::cargo_bin("pineappl")
-            .unwrap()
-            .args(&[
-                "optimize",
-                "data/LHCB_WP_7TEV.pineappl.lz4",
-                output.path().to_str().unwrap(),
-            ])
-            .assert()
-            .success()
-            .stdout("");
-    }
+    Command::cargo_bin("pineappl")
+        .unwrap()
+        .args(&[
+            "optimize",
+            "data/LHCB_WP_7TEV.pineappl.lz4",
+            output.path().to_str().unwrap(),
+        ])
+        .assert()
+        .success()
+        .stdout("");
+}
