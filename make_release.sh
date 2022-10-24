4e42eac64df663446e064eefe5f3d9b63c8f4ca6
Christopher Schwan <handgranaten-herbert@posteo.de>
2022-10-24 11:31:56 +0200

    Undo accidental changes to `make_release.sh`

--- make_release.sh
+++ make_release.sh
@@ -4,85 +4,85 @@ set -e
 
 version=$1
 
-#if [[ -z ${version} ]]; then
-#    echo "No version number given."
+if [[ -z ${version} ]]; then
+    echo "No version number given."
+    exit 1
+fi
+
+if [[ ! ${version} =~ ^[0-9]+.[0-9]+.[0-9]+$ ]]; then
+    echo "Version string incorrect."
+    exit 1
+fi
+
+if ! which gh >/dev/null; then
+    echo "Didn't find the \`gh\` binary."
+    exit 1
+fi
+
+if ! gh auth status 2>/dev/null; then
+    echo "Couldn't connect to the github repository."
+    exit 1
+fi
+
+if ! cargo msrv --help >/dev/null; then
+    echo "Didn't find \`msrv\` applet of \`cargo\`."
+    exit 1
+fi
+
+if ! cargo msrv --min 1.56.1 --max 1.56.1 >/dev/null; then
+    echo "Minimum supported Rust version doesn't match avertised one."
+    exit
+fi
+
+#if [[ -n $(git status --porcelain) ]]; then
+#    echo "This repository isn't clean. Make sure to add or delete the corresponding files."
 #    exit 1
 #fi
-#
-#if [[ ! ${version} =~ ^[0-9]+.[0-9]+.[0-9]+$ ]]; then
-#    echo "Version string incorrect."
+
+#if [[ ]]; then
+#    echo "You're not on master."
 #    exit 1
 #fi
-#
-#if ! which gh >/dev/null; then
-#    echo "Didn't find the \`gh\` binary."
-#    exit 1
-#fi
-#
-#if ! gh auth status 2>/dev/null; then
-#    echo "Couldn't connect to the github repository."
-#    exit 1
-#fi
-#
-#if ! cargo msrv --help >/dev/null; then
-#    echo "Didn't find \`msrv\` applet of \`cargo\`."
-#    exit 1
-#fi
-#
-#if ! cargo msrv --min 1.56.1 --max 1.56.1 >/dev/null; then
-#    echo "Minimum supported Rust version doesn't match avertised one."
-#    exit
-#fi
-#
-##if [[ -n $(git status --porcelain) ]]; then
-##    echo "This repository isn't clean. Make sure to add or delete the corresponding files."
-##    exit 1
-##fi
-#
-##if [[ ]]; then
-##    echo "You're not on master."
-##    exit 1
-##fi
-#
-#echo ">>> Testing release configuration with default features ..."
-#
-#cargo build --release
-#cargo test --release
-#
-#echo ">>> Testing release configuration with \`applgrid\` feature ..."
-#
-#cargo build --release --features=applgrid
-#cargo test --release --features=applgrid
-#
-#echo ">>> Testing release configuration with \`fastnlo\` feature ..."
-#
-#cargo build --release --features=fastnlo
-#cargo test --release --features=fastnlo
-#
-#echo ">>> Testing release configuration with \`fktable\` feature ..."
-#
-#cargo build --release --features=fktable
-#cargo test --release --features=fktable
-#
-#echo ">>> Testing if 'pineappl' can be published ..."
-#
-#cd pineappl
-#cargo publish --dry-run
-#cd ..
-#
-#echo ">>> Updating version strings ..."
-#
-#sed -i \
-#    -e "s:\(## \[Unreleased\]\):\1\n\n## [${version}] - $(date +%d/%m/%Y):" \
-#    -e "s:\[Unreleased\]\(\: https\://github.com/N3PDF/pineappl/compare/v\)\(.*\)...HEAD:[Unreleased]\1${version}...HEAD\n[${version}]\1\2...v${version}:" \
-#    CHANGELOG.md
-#
-#sed -i \
-#    -e "s:^version = \".*\":version = \"${version}\":" \
-#    -e "s:^\(pineappl = .*\)version = \".*\":\1version = \"${version}\":" \
-#    pineappl{,_capi,_cli,_py}/Cargo.toml
-#
-#echo ">>> Commiting and pushing changes ..."
+
+echo ">>> Testing release configuration with default features ..."
+
+cargo build --release
+cargo test --release
+
+echo ">>> Testing release configuration with \`applgrid\` feature ..."
+
+cargo build --release --features=applgrid
+cargo test --release --features=applgrid
+
+echo ">>> Testing release configuration with \`fastnlo\` feature ..."
+
+cargo build --release --features=fastnlo
+cargo test --release --features=fastnlo
+
+echo ">>> Testing release configuration with \`fktable\` feature ..."
+
+cargo build --release --features=fktable
+cargo test --release --features=fktable
+
+echo ">>> Testing if 'pineappl' can be published ..."
+
+cd pineappl
+cargo publish --dry-run
+cd ..
+
+echo ">>> Updating version strings ..."
+
+sed -i \
+    -e "s:\(## \[Unreleased\]\):\1\n\n## [${version}] - $(date +%d/%m/%Y):" \
+    -e "s:\[Unreleased\]\(\: https\://github.com/N3PDF/pineappl/compare/v\)\(.*\)...HEAD:[Unreleased]\1${version}...HEAD\n[${version}]\1\2...v${version}:" \
+    CHANGELOG.md
+
+sed -i \
+    -e "s:^version = \".*\":version = \"${version}\":" \
+    -e "s:^\(pineappl = .*\)version = \".*\":\1version = \"${version}\":" \
+    pineappl{,_capi,_cli,_py}/Cargo.toml
+
+echo ">>> Commiting and pushing changes ..."
 
 git commit -a -m "Release v${version}"
 git tag -a v${version} -m v${version}
