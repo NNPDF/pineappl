NNPDF/pineappl

commit c9fb4e3236a7744d59c70baeb2aa07f73ae0afa5

Make `helpers::convolute` more powerful

pineappl_cli/src/convolute.rs
@@ -1,4 +1,4 @@
-use super::helpers::{self, Subcommand};
+use super::helpers::{self, ConvoluteMode, Subcommand};
 use anyhow::Result;
 use clap::{Parser, ValueHint};
 use prettytable::{cell, Row};
@@ -65,8 +65,21 @@ impl Subcommand for Opts {
             &bins,
             &[],
             self.scales,
-            self.integrated,
+            if self.integrated {
+                ConvoluteMode::Integrated
+            } else {
+                ConvoluteMode::Normal
+            },
             self.force_positive,
+        );
+        let limits = helpers::convolute_limits(
+            &grid,
+            &bins,
+            if self.integrated {
+                ConvoluteMode::Integrated
+            } else {
+                ConvoluteMode::Normal
+            },
         );
 
         let other_results: Vec<f64> = self.pdfsets[1..]
@@ -80,18 +93,14 @@ impl Subcommand for Opts {
                     &bins,
                     &[],
                     1,
-                    self.integrated,
+                    if self.integrated {
+                        ConvoluteMode::Integrated
+                    } else {
+                        ConvoluteMode::Normal
+                    },
                     self.force_positive,
                 )
             })
-            .collect();
-
-        let bin_info = grid.bin_info();
-        let left_limits: Vec<_> = (0..bin_info.dimensions())
-            .map(|i| bin_info.left(i))
-            .collect();
-        let right_limits: Vec<_> = (0..bin_info.dimensions())
-            .map(|i| bin_info.right(i))
             .collect();
 
         let (x, y_label, y_unit) = helpers::labels_and_units(&grid, self.integrated);
@@ -121,7 +130,11 @@ impl Subcommand for Opts {
         let mut table = helpers::create_table();
         table.set_titles(title);
 
-        for (index, values) in results.chunks_exact(self.scales).enumerate() {
+        for (index, (limits, values)) in limits
+            .into_iter()
+            .zip(results.chunks_exact(self.scales))
+            .enumerate()
+        {
             let min_value = values
                 .iter()
                 .min_by(|left, right| left.partial_cmp(right).unwrap())
@@ -135,9 +148,9 @@ impl Subcommand for Opts {
             let row = table.add_empty_row();
 
             row.add_cell(cell!(r->format!("{}", bin)));
-            for (left, right) in left_limits.iter().zip(right_limits.iter()) {
-                row.add_cell(cell!(r->format!("{}", left[bin])));
-                row.add_cell(cell!(r->format!("{}", right[bin])));
+            for (left, right) in limits.iter() {
+                row.add_cell(cell!(r->format!("{}", left)));
+                row.add_cell(cell!(r->format!("{}", right)));
             }
             row.add_cell(cell!(r->format!("{:.*e}", self.digits_abs, values[0])));
 
@@ -150,13 +163,7 @@ impl Subcommand for Opts {
                 row.add_cell(cell!(r->format!("{:.*}", self.digits_rel, (max_value / values[0] - 1.0) * 100.0)));
             }
 
-            let bins = if bins.is_empty() {
-                bin_info.bins()
-            } else {
-                self.bins.len()
-            };
-
-            for &other in other_results.iter().skip(index).step_by(bins) {
+            for &other in other_results.iter().skip(index).step_by(results.len()) {
                 row.add_cell(cell!(r->format!("{:.*e}", self.digits_abs, other)));
                 row.add_cell(
                     cell!(r->format!("{:.*}", self.digits_rel, (other / values[0] - 1.0) * 100.0)),

pineappl/src/bin.rs
@@ -166,6 +166,27 @@ impl<'a> BinInfo<'a> {
         }
     }
 
+    /// For each bin return a vector of `(left, right)` limits for each dimension.
+    pub fn limits(&self) -> Vec<Vec<(f64, f64)>> {
+        self.remapper.map_or_else(
+            || {
+                self.limits
+                    .limits()
+                    .windows(2)
+                    .map(|window| vec![(window[0], window[1])])
+                    .collect()
+            },
+            |remapper| {
+                remapper
+                    .limits()
+                    .to_vec()
+                    .chunks_exact(self.dimensions())
+                    .map(<[(f64, f64)]>::to_vec)
+                    .collect()
+            },
+        )
+    }
+
     /// Returns all normalization factors.
     #[must_use]
     pub fn normalizations(&self) -> Vec<f64> {

pineappl_cli/src/channels.rs
@@ -1,4 +1,4 @@
-use super::helpers::{self, Subcommand};
+use super::helpers::{self, ConvoluteMode, Subcommand};
 use anyhow::Result;
 use clap::{Parser, ValueHint};
 use prettytable::{cell, Row};
@@ -68,7 +68,15 @@ impl Subcommand for Opts {
         let lumis = lumis;
 
         let limit = grid.lumi().len().min(self.limit);
-
+        let limits = helpers::convolute_limits(
+            &grid,
+            &[],
+            if self.integrated {
+                ConvoluteMode::Integrated
+            } else {
+                ConvoluteMode::Normal
+            },
+        );
         let results: Vec<_> = (0..grid.lumi().len())
             .map(|lumi| {
                 let mut lumi_mask = vec![false; grid.lumi().len()];
@@ -80,18 +88,14 @@ impl Subcommand for Opts {
                     &[],
                     &lumi_mask,
                     1,
-                    self.integrated,
+                    if self.integrated {
+                        ConvoluteMode::Integrated
+                    } else {
+                        ConvoluteMode::Normal
+                    },
                     self.force_positive,
                 )
             })
-            .collect();
-
-        let bin_info = grid.bin_info();
-        let left_limits: Vec<_> = (0..bin_info.dimensions())
-            .map(|i| bin_info.left(i))
-            .collect();
-        let right_limits: Vec<_> = (0..bin_info.dimensions())
-            .map(|i| bin_info.right(i))
             .collect();
 
         let (x, y_label, y_unit) = helpers::labels_and_units(&grid, self.integrated);
@@ -112,14 +116,14 @@ impl Subcommand for Opts {
         let mut table = helpers::create_table();
         table.set_titles(title);
 
-        for bin in 0..bin_info.bins() {
+        for (bin, limits) in limits.iter().enumerate() {
             let row = table.add_empty_row();
 
             row.add_cell(cell!(r->format!("{}", bin)));
 
-            for (left, right) in left_limits.iter().zip(right_limits.iter()) {
-                row.add_cell(cell!(r->format!("{}", left[bin])));
-                row.add_cell(cell!(r->format!("{}", right[bin])));
+            for (left, right) in limits {
+                row.add_cell(cell!(r->format!("{}", left)));
+                row.add_cell(cell!(r->format!("{}", right)));
             }
 
             if self.absolute {

pineappl_cli/src/diff.rs
@@ -1,4 +1,4 @@
-use super::helpers::{self, Subcommand};
+use super::helpers::{self, ConvoluteMode, Subcommand};
 use anyhow::{bail, Result};
 use clap::{Parser, ValueHint};
 use prettytable::{cell, Row};
@@ -130,22 +130,15 @@ impl Subcommand for Opts {
         let mut pdf = helpers::create_pdf(&self.pdfset)?;
 
         let mut table = helpers::create_table();
-        let bin_info = grid1.bin_info();
-
-        let left_limits: Vec<_> = (0..bin_info.dimensions())
-            .map(|i| bin_info.left(i))
-            .collect();
-        let right_limits: Vec<_> = (0..bin_info.dimensions())
-            .map(|i| bin_info.right(i))
-            .collect();
-
         let mut title = Row::empty();
         title.add_cell(cell!(c->"b"));
-        for i in 0..bin_info.dimensions() {
+        for i in 0..grid1.bin_info().dimensions() {
             let mut cell = cell!(c->format!("x{}", i + 1));
             cell.set_hspan(2);
             title.add_cell(cell);
         }
+
+        let limits1 = helpers::convolute_limits(&grid1, &[], ConvoluteMode::Normal);
 
         if self.ignore_orders {
             let mut cell = cell!(c->"diff");
@@ -161,7 +154,7 @@ impl Subcommand for Opts {
                 &[],
                 &[],
                 1,
-                false,
+                ConvoluteMode::Normal,
                 self.force_positive,
             );
             let results2 = helpers::convolute(
@@ -171,17 +164,21 @@ impl Subcommand for Opts {
                 &[],
                 &[],
                 1,
-                false,
+                ConvoluteMode::Normal,
                 self.force_positive,
             );
 
-            for (bin, (result1, result2)) in results1.iter().zip(results2.iter()).enumerate() {
+            for (bin, (limits1, (result1, result2))) in limits1
+                .iter()
+                .zip(results1.iter().zip(results2.iter()))
+                .enumerate()
+            {
                 let row = table.add_empty_row();
 
                 row.add_cell(cell!(r->bin));
-                for (left, right) in left_limits.iter().zip(right_limits.iter()) {
-                    row.add_cell(cell!(r->format!("{}", left[bin])));
-                    row.add_cell(cell!(r->format!("{}", right[bin])));
+                for (left, right) in limits1.iter() {
+                    row.add_cell(cell!(r->format!("{}", left)));
+                    row.add_cell(cell!(r->format!("{}", right)));
                 }
 
                 let result1 = result1 * self.scale1;
@@ -213,7 +210,7 @@ impl Subcommand for Opts {
                         &[],
                         &[],
                         1,
-                        false,
+                        ConvoluteMode::Normal,
                         self.force_positive,
                     )
                 })
@@ -228,19 +225,19 @@ impl Subcommand for Opts {
                         &[],
                         &[],
                         1,
-                        false,
+                        ConvoluteMode::Normal,
                         self.force_positive,
                     )
                 })
                 .collect();
 
-            for bin in 0..bin_info.bins() {
+            for (bin, limits1) in limits1.iter().enumerate() {
                 let row = table.add_empty_row();
 
                 row.add_cell(cell!(r->bin));
-                for (left, right) in left_limits.iter().zip(right_limits.iter()) {
-                    row.add_cell(cell!(r->format!("{}", left[bin])));
-                    row.add_cell(cell!(r->format!("{}", right[bin])));
+                for (left, right) in limits1 {
+                    row.add_cell(cell!(r->format!("{}", left)));
+                    row.add_cell(cell!(r->format!("{}", right)));
                 }
 
                 for (result1, result2) in order_results1.iter().zip(order_results2.iter()) {

pineappl_cli/src/helpers.rs
@@ -121,6 +121,11 @@ pub fn labels_and_units(grid: &Grid, integrated: bool) -> (Vec<(String, &str)>, 
     )
 }
 
+pub enum ConvoluteMode {
+    Integrated,
+    Normal,
+}
+
 pub fn convolute(
     grid: &Grid,
     lhapdf: &mut Pdf,
@@ -128,7 +133,7 @@ pub fn convolute(
     bins: &[usize],
     lumis: &[bool],
     scales: usize,
-    integrated: bool,
+    mode: ConvoluteMode,
     force_positive: bool,
 ) -> Vec<f64> {
     let orders: Vec<_> = grid
@@ -164,25 +169,36 @@ pub fn convolute(
     };
     let mut alphas = |q2| lhapdf.alphas_q2(q2);
     let mut cache = LumiCache::with_one(pdf_pdg_id, &mut pdf, &mut alphas);
-
     let mut results = grid.convolute(&mut cache, &orders, bins, lumis, &SCALES_VECTOR[0..scales]);
 
-    if integrated {
-        let normalizations = grid.bin_info().normalizations();
+    match mode {
+        ConvoluteMode::Integrated => {
+            let normalizations = grid.bin_info().normalizations();
 
-        results
-            .iter_mut()
-            .zip(
-                normalizations
-                    .iter()
-                    .enumerate()
-                    .filter(|(index, _)| (bins.is_empty() || bins.iter().any(|b| b == index)))
-                    .flat_map(|(_, norm)| iter::repeat(norm).take(scales)),
-            )
-            .for_each(|(value, norm)| *value *= norm);
+            results
+                .iter_mut()
+                .zip(
+                    normalizations
+                        .iter()
+                        .enumerate()
+                        .filter(|(index, _)| (bins.is_empty() || bins.contains(index)))
+                        .flat_map(|(_, norm)| iter::repeat(norm).take(scales)),
+                )
+                .for_each(|(value, norm)| *value *= norm);
+        }
+        ConvoluteMode::Normal => {}
     }
 
     results
+}
+
+pub fn convolute_limits(grid: &Grid, bins: &[usize], _: ConvoluteMode) -> Vec<Vec<(f64, f64)>> {
+    grid.bin_info()
+        .limits()
+        .into_iter()
+        .enumerate()
+        .filter_map(|(index, limits)| (bins.is_empty() || bins.contains(&index)).then_some(limits))
+        .collect()
 }
 
 pub fn convolute_subgrid(

pineappl_cli/src/import.rs
@@ -1,4 +1,4 @@
-use super::helpers::{self, Subcommand};
+use super::helpers::{self, ConvoluteMode, Subcommand};
 use anyhow::{anyhow, Result};
 use clap::{Parser, ValueHint};
 use pineappl::grid::Grid;
@@ -165,7 +165,16 @@ impl Subcommand for Opts {
             println!("file was converted, but we cannot check the conversion for this type");
         } else {
             let mut pdf = helpers::create_pdf(&self.pdfset)?;
-            let results = helpers::convolute(&grid, &mut pdf, &[], &[], &[], 1, false, false);
+            let results = helpers::convolute(
+                &grid,
+                &mut pdf,
+                &[],
+                &[],
+                &[],
+                1,
+                ConvoluteMode::Normal,
+                false,
+            );
 
             let mut table = helpers::create_table();
             table.set_titles(row![c => "b", "PineAPPL", grid_type, "rel. diff"]);

pineappl_cli/src/orders.rs
@@ -1,4 +1,4 @@
-use super::helpers::{self, Subcommand};
+use super::helpers::{self, ConvoluteMode, Subcommand};
 use anyhow::Result;
 use clap::{Parser, ValueHint};
 use prettytable::{cell, Row};
@@ -53,6 +53,15 @@ impl Subcommand for Opts {
         orders.sort();
         let orders = orders;
 
+        let limits = helpers::convolute_limits(
+            &grid,
+            &[],
+            if self.integrated {
+                ConvoluteMode::Integrated
+            } else {
+                ConvoluteMode::Normal
+            },
+        );
         let results: Vec<Vec<f64>> = orders
             .iter()
             .map(|order| {
@@ -63,7 +72,11 @@ impl Subcommand for Opts {
                     &[],
                     &[],
                     1,
-                    self.integrated,
+                    if self.integrated {
+                        ConvoluteMode::Integrated
+                    } else {
+                        ConvoluteMode::Normal
+                    },
                     self.force_positive,
                 )
             })
@@ -73,14 +86,6 @@ impl Subcommand for Opts {
             let order = orders.first().unwrap();
             order.alphas + order.alpha
         };
-
-        let bin_info = grid.bin_info();
-        let left_limits: Vec<_> = (0..bin_info.dimensions())
-            .map(|i| bin_info.left(i))
-            .collect();
-        let right_limits: Vec<_> = (0..bin_info.dimensions())
-            .map(|i| bin_info.right(i))
-            .collect();
 
         let (x, y_label, y_unit) = helpers::labels_and_units(&grid, self.integrated);
         let mut title = Row::empty();
@@ -99,13 +104,13 @@ impl Subcommand for Opts {
         let mut table = helpers::create_table();
         table.set_titles(title);
 
-        for bin in 0..bin_info.bins() {
+        for (bin, limits) in limits.iter().enumerate() {
             let row = table.add_empty_row();
 
             row.add_cell(cell!(r->format!("{}", bin)));
-            for (left, right) in left_limits.iter().zip(right_limits.iter()) {
-                row.add_cell(cell!(r->format!("{}", left[bin])));
-                row.add_cell(cell!(r->format!("{}", right[bin])));
+            for (left, right) in limits {
+                row.add_cell(cell!(r->format!("{}", left)));
+                row.add_cell(cell!(r->format!("{}", right)));
             }
             row.add_cell(cell!(r->format!("{:.*e}", self.digits_abs,
             results.iter().fold(0.0, |value, results| value + results[bin]))));

pineappl_cli/src/pdfunc.rs
@@ -1,4 +1,4 @@
-use super::helpers::{self, Subcommand};
+use super::helpers::{self, ConvoluteMode, Subcommand};
 use anyhow::Result;
 use clap::{Parser, ValueHint};
 use prettytable::{cell, Row};
@@ -55,6 +55,15 @@ impl Subcommand for Opts {
             .build_global()
             .unwrap();
 
+        let limits = helpers::convolute_limits(
+            &grid,
+            &[],
+            if self.integrated {
+                ConvoluteMode::Integrated
+            } else {
+                ConvoluteMode::Normal
+            },
+        );
         let results: Vec<f64> = pdfs
             .into_par_iter()
             .flat_map(|mut pdf| {
@@ -65,18 +74,14 @@ impl Subcommand for Opts {
                     &[],
                     &[],
                     1,
-                    self.integrated,
+                    if self.integrated {
+                        ConvoluteMode::Integrated
+                    } else {
+                        ConvoluteMode::Normal
+                    },
                     self.force_positive,
                 )
             })
-            .collect();
-
-        let bin_info = grid.bin_info();
-        let left_limits: Vec<_> = (0..bin_info.dimensions())
-            .map(|i| bin_info.left(i))
-            .collect();
-        let right_limits: Vec<_> = (0..bin_info.dimensions())
-            .map(|i| bin_info.right(i))
             .collect();
 
         let (x, y_label, y_unit) = helpers::labels_and_units(&grid, self.integrated);
@@ -93,11 +98,11 @@ impl Subcommand for Opts {
         let mut table = helpers::create_table();
         table.set_titles(title);
 
-        for bin in 0..bin_info.bins() {
+        for (bin, left_right_limits) in limits.iter().enumerate() {
             let values: Vec<_> = results
                 .iter()
                 .skip(bin)
-                .step_by(bin_info.bins())
+                .step_by(limits.len())
                 .copied()
                 .collect();
             let uncertainty = set.uncertainty(&values, self.cl, false)?;
@@ -105,9 +110,9 @@ impl Subcommand for Opts {
             let row = table.add_empty_row();
 
             row.add_cell(cell!(r->format!("{}", bin)));
-            for (left, right) in left_limits.iter().zip(right_limits.iter()) {
-                row.add_cell(cell!(r->format!("{}", left[bin])));
-                row.add_cell(cell!(r->format!("{}", right[bin])));
+            for (left, right) in left_right_limits {
+                row.add_cell(cell!(r->format!("{}", left)));
+                row.add_cell(cell!(r->format!("{}", right)));
             }
             row.add_cell(cell!(r->format!("{:.*e}", self.digits_abs, member.map_or(uncertainty.central, |member| values[member]))));
             row.add_cell(

commit 9d58ea1becc23f135df2759d43d34f716af97f16

Fix `pineappl convolute` not showing more than two PDF sets
This bug was introduced with commit c9fb4e32

pineappl_cli/src/convolute.rs
@@ -85,6 +85,7 @@ impl Subcommand for Opts {
                 ConvoluteMode::Normal
             },
         );
+        let bin_count = limits.len();
 
         let other_results: Vec<_> = self.pdfsets[1..]
             .iter()
@@ -167,7 +168,7 @@ impl Subcommand for Opts {
                 row.add_cell(cell!(r->format!("{:.*}", self.digits_rel, (max_value / values[0] - 1.0) * 100.0)));
             }
 
-            for &other in other_results.iter().skip(index).step_by(results.len()) {
+            for &other in other_results.iter().skip(index).step_by(bin_count) {
                 row.add_cell(cell!(r->format!("{:.*e}", self.digits_abs, other)));
                 row.add_cell(
                     cell!(r->format!("{:.*}", self.digits_rel, (other / values[0] - 1.0) * 100.0)),

pineappl_cli/tests/convolute.rs
@@ -87,6 +87,19 @@ const MULTIPLE_PDFS_WITH_RELABELING_STR: &str =
 7    4  4.5 1.3772029e1    -3.46     2.85 1.3640796e1 -0.95
 ";
 
+const THREE_PDFS_STR: &str = "b   etal    disg/detal  scale uncertainty NNPDF31_nlo_as_0118_luxqed/1  NNPDF31_nlo_as_0118_luxqed/2 
+     []        [pb]            [%]                  [pb] [%]                      [pb] [%]           
+-+----+----+-----------+--------+--------+--------------+--------------+--------------+--------------
+0    2 2.25 3.7527620e2    -3.77     2.71    3.7379477e2          -0.39    3.7670804e2           0.38
+1 2.25  2.5 3.4521553e2    -3.79     2.80    3.4316002e2          -0.60    3.4596819e2           0.22
+2  2.5 2.75 3.0001406e2    -3.78     2.86    2.9780437e2          -0.74    3.0023944e2           0.08
+3 2.75    3 2.4257663e2    -3.77     2.92    2.4059099e2          -0.82    2.4259549e2           0.01
+4    3 3.25 1.8093343e2    -3.74     2.95    1.7941535e2          -0.84    1.8095662e2           0.01
+5 3.25  3.5 1.2291115e2    -3.71     2.98    1.2195463e2          -0.78    1.2308878e2           0.14
+6  3.5    4 5.7851018e1    -3.63     2.97    5.7551676e1          -0.52    5.8229565e1           0.65
+7    4  4.5 1.3772029e1    -3.46     2.85    1.3640796e1          -0.95    1.4234097e1           3.36
+";
+
 const WRONG_LHAID_STR: &str =
     "error: Invalid value '0' for '<PDFSETS>...': The PDF set for the LHAPDF ID `0` was not found
 
@@ -282,6 +295,23 @@ fn multiple_pdfs_with_relabeling() {
         .assert()
         .success()
         .stdout(MULTIPLE_PDFS_WITH_RELABELING_STR);
+}
+
+#[test]
+fn three_pdfs() {
+    Command::cargo_bin("pineappl")
+        .unwrap()
+        .args(&[
+            "--silence-lhapdf",
+            "convolute",
+            "data/LHCB_WP_7TEV.pineappl.lz4",
+            "NNPDF31_nlo_as_0118_luxqed/0",
+            "NNPDF31_nlo_as_0118_luxqed/1",
+            "NNPDF31_nlo_as_0118_luxqed/2",
+        ])
+        .assert()
+        .success()
+        .stdout(THREE_PDFS_STR);
 }
 
 #[test]