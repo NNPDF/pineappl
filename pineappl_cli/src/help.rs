70b710110107790fdaaddb8138ef6893f57c5d26
Christopher Schwan <handgranaten-herbert@posteo.de>
2023-02-01 15:13:38 +0100

    Improve error handling in `pineappl help`

--- pineappl_cli/src/help.rs
+++ pineappl_cli/src/help.rs
@@ -5,6 +5,19 @@ use clap_mangen::Man;
 use std::io::Write;
 use std::process::{Command, ExitCode, Stdio};
 
+fn find_subcommand(cmd: clap::Command, args: &[String]) -> Option<clap::Command> {
+    if args.is_empty() {
+        Some(cmd)
+    } else {
+        let mut iter = args.iter();
+        let mut cmd = cmd;
+        while let Some(s) = iter.next() {
+            cmd = cmd.find_subcommand(s)?.clone();
+        }
+        Some(cmd)
+    }
+}
+
 /// Display a manpage for selected subcommands.
 #[derive(Parser)]
 pub struct Opts {
@@ -12,37 +25,34 @@ pub struct Opts {
     subcommand: Vec<String>,
 }
 
+// TODO: installing manpages requires <https://github.com/rust-lang/cargo/issues/2729>, but we
+// could go ahead and install them ourselves to /path/of/pineappl/../share/man/man1/pineappl.1.gz
+
 impl Subcommand for Opts {
     fn run(&self, _: &GlobalConfiguration) -> Result<ExitCode> {
-        let cmd = crate::Opts::command();
-        let subcmd = if self.subcommand.is_empty() {
-            cmd
-        } else {
-            let mut iter = self.subcommand.iter();
-            let mut cmd = cmd;
-            while let Some(s) = iter.next() {
-                // TODO: if `unwrap` fails the arguments to `help` are wrong
-                cmd = cmd.find_subcommand(s).unwrap().clone();
-            }
-            cmd
-        };
-        let man = Man::new(subcmd);
+        // TODO: SYNOPSIS doesn't show top-level subcommands: instead of `pineappl-convolute` only
+        // `convolute` is shown
+
+        // TODO: VERSION shows the version with two `v` in front of it
+
+        let mut cmd = crate::Opts::command();
+        cmd.build();
+
+        // TODO: if `unwrap` fails the arguments were wrong
+        let man = Man::new(find_subcommand(cmd, &self.subcommand).unwrap());
         let mut buffer = Vec::new();
 
         man.render(&mut buffer)?;
 
-        // TODO: check remaining `unwrap`s
-
-        // TODO: is this the best way? What do we do when `man` isn't available (Windows, ...)?
+        // TODO: if `man` can't be found display the usual `--help` message
         let mut child = Command::new("man")
             .args(["-l", "-"])
             .stdin(Stdio::piped())
             .stdout(Stdio::inherit())
-            .spawn()
-            .unwrap();
+            .spawn()?;
 
-        child.stdin.take().unwrap().write_all(&buffer).unwrap();
-        child.wait().unwrap();
+        child.stdin.take().unwrap().write_all(&buffer)?;
+        child.wait()?;
 
         Ok(ExitCode::SUCCESS)
     }
