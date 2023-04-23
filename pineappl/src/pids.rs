cee9357cff382142cc9e17c2f1c7b011141d6b5f
Christopher Schwan <handgranaten-herbert@posteo.de>
2023-04-23 15:08:05 +0200

    Add new function `pdg_mc_ids_to_evol`

--- pineappl/src/pids.rs
+++ pineappl/src/pids.rs
@@ -1,5 +1,7 @@
 //! TODO
 
+const EVOL_BASIS_IDS: [i32; 12] = [100, 103, 108, 115, 124, 135, 200, 203, 208, 215, 224, 235];
+
 /// Translates IDs from the evolution basis into IDs using PDG Monte Carlo IDs.
 #[must_use]
 pub fn evol_to_pdg_mc_ids(id: i32) -> Vec<(i32, f64)> {
@@ -149,6 +151,39 @@ pub fn charge_conjugate(lumi_id_types: &str, pid: i32) -> (i32, f64) {
         ("evol" | "pdg_mc_ids", _) => (charge_conjugate_pdg_pid(pid), 1.0),
         _ => todo!(),
     }
+}
+
+/// Given `tuples` represting a linear combination of PDG MC IDs, return a PID for the `evol`
+/// basis. The order of each tuple in `tuples` is not relevant. This function inverts
+/// [`evol_to_pdg_mc_ids`]. If the inversion is not possible, `None` is returned.
+pub fn pdg_mc_ids_to_evol(tuples: &[(i32, f64)]) -> Option<i32> {
+    let mut tuples = tuples.to_vec();
+    tuples.retain(|&(_, factor)| factor != 0.0);
+    tuples.sort_by_key(|&(id, _)| id);
+    let tuples = tuples;
+
+    for &evol_pid in &EVOL_BASIS_IDS {
+        let mut evol_vec = evol_to_pdg_mc_ids(evol_pid);
+        evol_vec.sort_by_key(|&(id, _)| id);
+        let evol_vec = evol_vec;
+
+        if evol_vec == tuples {
+            return Some(evol_pid);
+        }
+    }
+
+    let non_zero: Vec<_> = tuples
+        .into_iter()
+        .filter(|&(_, factor)| factor != 0.0)
+        .collect();
+
+    if let &[(pid, factor)] = non_zero.as_slice() {
+        if factor == 1.0 {
+            return Some(pid);
+        }
+    }
+
+    None
 }
 
 #[cfg(test)]
@@ -339,6 +374,305 @@ mod tests {
                 (6, -5.0),
                 (-6, 5.0),
             ],
+        );
+    }
+
+    #[test]
+    fn test_pdg_mc_ids_to_evol() {
+        assert_eq!(pdg_mc_ids_to_evol(&[]), None);
+
+        // check photon
+        assert_eq!(
+            pdg_mc_ids_to_evol(&[
+                (22, 1.0),
+                (-6, 0.0),
+                (-5, 0.0),
+                (-4, 0.0),
+                (-3, 0.0),
+                (-2, 0.0),
+                (-1, 0.0),
+                (21, 0.0),
+                (1, 0.0),
+                (2, 0.0),
+                (3, 0.0),
+                (4, 0.0),
+                (5, 0.0),
+                (6, 0.0),
+            ]),
+            Some(22)
+        );
+
+        // check gluon
+        assert_eq!(
+            pdg_mc_ids_to_evol(&[
+                (22, 0.0),
+                (-6, 0.0),
+                (-5, 0.0),
+                (-4, 0.0),
+                (-3, 0.0),
+                (-2, 0.0),
+                (-1, 0.0),
+                (21, 1.0),
+                (1, 0.0),
+                (2, 0.0),
+                (3, 0.0),
+                (4, 0.0),
+                (5, 0.0),
+                (6, 0.0),
+            ]),
+            Some(21)
+        );
+
+        // check singlet
+        assert_eq!(
+            pdg_mc_ids_to_evol(&[
+                (22, 0.0),
+                (-6, 1.0),
+                (-5, 1.0),
+                (-4, 1.0),
+                (-3, 1.0),
+                (-2, 1.0),
+                (-1, 1.0),
+                (21, 0.0),
+                (1, 1.0),
+                (2, 1.0),
+                (3, 1.0),
+                (4, 1.0),
+                (5, 1.0),
+                (6, 1.0),
+            ]),
+            Some(100)
+        );
+
+        // check T3
+        assert_eq!(
+            pdg_mc_ids_to_evol(&[
+                (22, 0.0),
+                (-6, 0.0),
+                (-5, 0.0),
+                (-4, 0.0),
+                (-3, 0.0),
+                (-2, 1.0),
+                (-1, -1.0),
+                (21, 0.0),
+                (1, -1.0),
+                (2, 1.0),
+                (3, 0.0),
+                (4, 0.0),
+                (5, 0.0),
+                (6, 0.0),
+            ]),
+            Some(103)
+        );
+
+        // check T8
+        assert_eq!(
+            pdg_mc_ids_to_evol(&[
+                (22, 0.0),
+                (-6, 0.0),
+                (-5, 0.0),
+                (-4, 0.0),
+                (-3, -2.0),
+                (-2, 1.0),
+                (-1, 1.0),
+                (21, 0.0),
+                (1, 1.0),
+                (2, 1.0),
+                (3, -2.0),
+                (4, 0.0),
+                (5, 0.0),
+                (6, 0.0),
+            ]),
+            Some(108)
+        );
+
+        // check T15
+        assert_eq!(
+            pdg_mc_ids_to_evol(&[
+                (22, 0.0),
+                (-6, 0.0),
+                (-5, 0.0),
+                (-4, -3.0),
+                (-3, 1.0),
+                (-2, 1.0),
+                (-1, 1.0),
+                (21, 0.0),
+                (1, 1.0),
+                (2, 1.0),
+                (3, 1.0),
+                (4, -3.0),
+                (5, 0.0),
+                (6, 0.0),
+            ]),
+            Some(115)
+        );
+
+        // check T24
+        assert_eq!(
+            pdg_mc_ids_to_evol(&[
+                (22, 0.0),
+                (-6, 0.0),
+                (-5, -4.0),
+                (-4, 1.0),
+                (-3, 1.0),
+                (-2, 1.0),
+                (-1, 1.0),
+                (21, 0.0),
+                (1, 1.0),
+                (2, 1.0),
+                (3, 1.0),
+                (4, 1.0),
+                (5, -4.0),
+                (6, 0.0),
+            ]),
+            Some(124)
+        );
+
+        // check T35
+        assert_eq!(
+            pdg_mc_ids_to_evol(&[
+                (22, 0.0),
+                (-6, -5.0),
+                (-5, 1.0),
+                (-4, 1.0),
+                (-3, 1.0),
+                (-2, 1.0),
+                (-1, 1.0),
+                (21, 0.0),
+                (1, 1.0),
+                (2, 1.0),
+                (3, 1.0),
+                (4, 1.0),
+                (5, 1.0),
+                (6, -5.0),
+            ]),
+            Some(135)
+        );
+
+        // check valence
+        assert_eq!(
+            pdg_mc_ids_to_evol(&[
+                (22, 0.0),
+                (-6, -1.0),
+                (-5, -1.0),
+                (-4, -1.0),
+                (-3, -1.0),
+                (-2, -1.0),
+                (-1, -1.0),
+                (21, 0.0),
+                (1, 1.0),
+                (2, 1.0),
+                (3, 1.0),
+                (4, 1.0),
+                (5, 1.0),
+                (6, 1.0),
+            ]),
+            Some(200)
+        );
+
+        // check V3
+        assert_eq!(
+            pdg_mc_ids_to_evol(&[
+                (22, 0.0),
+                (-6, 0.0),
+                (-5, 0.0),
+                (-4, 0.0),
+                (-3, 0.0),
+                (-2, -1.0),
+                (-1, 1.0),
+                (21, 0.0),
+                (1, -1.0),
+                (2, 1.0),
+                (3, 0.0),
+                (4, 0.0),
+                (5, 0.0),
+                (6, 0.0),
+            ]),
+            Some(203)
+        );
+
+        // check V8
+        assert_eq!(
+            pdg_mc_ids_to_evol(&[
+                (22, 0.0),
+                (-6, 0.0),
+                (-5, 0.0),
+                (-4, 0.0),
+                (-3, 2.0),
+                (-2, -1.0),
+                (-1, -1.0),
+                (21, 0.0),
+                (1, 1.0),
+                (2, 1.0),
+                (3, -2.0),
+                (4, 0.0),
+                (5, 0.0),
+                (6, 0.0),
+            ]),
+            Some(208)
+        );
+
+        // check V15
+        assert_eq!(
+            pdg_mc_ids_to_evol(&[
+                (22, 0.0),
+                (-6, 0.0),
+                (-5, 0.0),
+                (-4, 3.0),
+                (-3, -1.0),
+                (-2, -1.0),
+                (-1, -1.0),
+                (21, 0.0),
+                (1, 1.0),
+                (2, 1.0),
+                (3, 1.0),
+                (4, -3.0),
+                (5, 0.0),
+                (6, 0.0),
+            ]),
+            Some(215)
+        );
+
+        // check V24
+        assert_eq!(
+            pdg_mc_ids_to_evol(&[
+                (22, 0.0),
+                (-6, 0.0),
+                (-5, 4.0),
+                (-4, -1.0),
+                (-3, -1.0),
+                (-2, -1.0),
+                (-1, -1.0),
+                (21, 0.0),
+                (1, 1.0),
+                (2, 1.0),
+                (3, 1.0),
+                (4, 1.0),
+                (5, -4.0),
+                (6, 0.0),
+            ]),
+            Some(224)
+        );
+
+        // check V35
+        assert_eq!(
+            pdg_mc_ids_to_evol(&[
+                (22, 0.0),
+                (-6, 5.0),
+                (-5, -1.0),
+                (-4, -1.0),
+                (-3, -1.0),
+                (-2, -1.0),
+                (-1, -1.0),
+                (21, 0.0),
+                (1, 1.0),
+                (2, 1.0),
+                (3, 1.0),
+                (4, 1.0),
+                (5, 1.0),
+                (6, -5.0),
+            ]),
+            Some(235)
         );
     }
 }
