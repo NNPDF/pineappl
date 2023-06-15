6559a847f322ddb6c33593d84daa80d4d85a05bc
Christopher Schwan <handgranaten-herbert@posteo.de>
2023-06-15 09:49:31 +0200

    Test `merge` by actually merging two grids together

--- pineappl_cli/tests/merge.rs
+++ pineappl_cli/tests/merge.rs
@@ -13,17 +13,17 @@ Options:
   -h, --help  Print help
 ";
 
-const DEFAULT_STR: &str = "b   etal    disg/detal  scale uncertainty
+const DEFAULT_STR: &str = "b   etal    dsig/detal  scale uncertainty
      []        [pb]            [%]       
 -+----+----+-----------+--------+--------
-0    2 2.25 3.7527620e2    -3.77     2.71
-1 2.25  2.5 3.4521553e2    -3.79     2.80
-2  2.5 2.75 3.0001406e2    -3.78     2.86
-3 2.75    3 2.4257663e2    -3.77     2.92
-4    3 3.25 1.8093343e2    -3.74     2.95
-5 3.25  3.5 1.2291115e2    -3.71     2.98
-6  3.5    4 5.7851018e1    -3.63     2.97
-7    4  4.5 1.3772029e1    -3.46     2.85
+0    2 2.25 1.5769498e3    -3.75     2.72
+1 2.25  2.5 1.4412311e3    -3.75     2.78
+2  2.5 2.75 1.2505273e3    -3.77     2.86
+3 2.75    3 1.0077177e3    -3.75     2.90
+4    3 3.25 7.4800340e2    -3.72     2.93
+5 3.25  3.5 5.0601781e2    -3.69     2.96
+6  3.5    4 2.3818927e2    -3.63     2.96
+7    4  4.5 5.8009213e1    -3.59     2.95
 ";
 
 #[test]
@@ -45,7 +45,8 @@ fn default() {
         .args([
             "merge",
             output.path().to_str().unwrap(),
-            "data/LHCB_WP_7TEV.pineappl.lz4",
+            "../test-data/LHCB_WP_7TEV.pineappl.lz4",
+            "../test-data/LHCB_WP_7TEV.pineappl.lz4",
         ])
         .assert()
         .success()
@@ -57,7 +58,7 @@ fn default() {
             "--silence-lhapdf",
             "convolute",
             output.path().to_str().unwrap(),
-            "NNPDF31_nlo_as_0118_luxqed",
+            "NNPDF40_nnlo_as_01180",
         ])
         .assert()
         .success()
