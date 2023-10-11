4df6ce6d70bab10b229b25aa3f6c67c5c2e0955b
Alessandro Candido <candido.ale@gmail.com>
2023-10-11 19:11:55 +0200

    Add maturin, update nixos distribution

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     systems.url = "github:nix-systems/default";
     devenv.url = "github:cachix/devenv";
     nixpkgs-python.url = "github:cachix/nixpkgs-python";
@@ -8,38 +8,43 @@
     fenix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
+  outputs = {
+    self,
+    nixpkgs,
+    devenv,
+    systems,
+    ...
+  } @ inputs: let
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
+  in {
+    devShells =
+      forEachSystem
+      (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            {
+              packages = with pkgs; [maturin];
+
+              languages.python = {
+                enable = true;
+                venv.enable = true;
+                version = "3.11";
+              };
+              languages.rust = {
+                enable = true;
+                channel = "stable";
+              };
+            }
+          ];
+        };
+      });
+  };
+
   nixConfig = {
     extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
     extra-substituters = "https://devenv.cachix.org";
   };
-
-  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
-    let
-      forEachSystem = nixpkgs.lib.genAttrs (import systems);
-    in
-    {
-      devShells = forEachSystem
-        (system:
-          let
-            pkgs = nixpkgs.legacyPackages.${system};
-          in
-          {
-            default = devenv.lib.mkShell {
-              inherit inputs pkgs;
-              modules = [
-                {
-                  languages.python = {
-                    enable = true;
-                    venv.enable = true;
-                    version = "3.11";
-                  };
-                  languages.rust = {
-                    enable = true;
-                    channel = "stable";
-                  };
-                }
-              ];
-            };
-          });
-    };
 }
