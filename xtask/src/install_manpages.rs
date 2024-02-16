3aaeb3286d77d79f4dbeb34fa1c41ee23e2c3376
Christopher Schwan <handgranaten-herbert@posteo.de>
2024-02-16 15:59:27 +0100

    Use `clap` for `xtask`

---
+++ xtask/src/install_manpages.rs
@@ -0,0 +1,61 @@
+use super::Subcommand;
+use anyhow::Result;
+use clap::CommandFactory;
+use clap::{Parser, ValueHint};
+use clap_mangen::Man;
+use std::fs::File;
+use std::io::BufWriter;
+use std::path::{Path, PathBuf};
+
+/// Install PineAPPL's manpages.
+#[derive(Parser)]
+pub struct Opts {
+    /// Path to where the directories should installed.
+    #[arg(value_hint = ValueHint::DirPath)]
+    directory: PathBuf,
+}
+
+fn render_manpages(path: &Path, cmd: &clap::Command, version: &str) -> Result<()> {
+    let name = cmd
+        .get_bin_name()
+        .unwrap_or(cmd.get_name())
+        .replace(" ", "-");
+
+    Man::new(cmd.clone())
+        // pass space, otherwise the ordering of the remaining arguments is incorrect
+        .date(" ")
+        .manual("PineAPPL CLI Manual")
+        .source(format!("PineAPPL {version}"))
+        .title(name.to_ascii_uppercase())
+        .render(&mut BufWriter::new(File::create(
+            path.join(format!("{name}.1")),
+        )?))?;
+
+    for subcmd in cmd.get_subcommands() {
+        render_manpages(path, subcmd, version)?;
+    }
+
+    Ok(())
+}
+
+impl Subcommand for Opts {
+    fn run(&self) -> Result<()> {
+        let cmd = pineappl_cli::Opts::command();
+        let version: String = cmd
+            .get_version()
+            // UNWRAP: the command must have a version
+            .unwrap()
+            .strip_prefix('v')
+            // UNWRAP: the version string must start with a 'v'
+            .unwrap()
+            .to_string();
+        let mut cmd = cmd.version(version.clone());
+
+        // this is needed so subcommands return the correct `bin_name`
+        cmd.build();
+
+        render_manpages(&self.directory, &cmd, &version)?;
+
+        Ok(())
+    }
+}
--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,60 +1,29 @@
 use anyhow::Result;
-use std::env;
-use std::path::Path;
+use clap::Parser;
+use enum_dispatch::enum_dispatch;
 
-fn main() -> Result<()> {
-    let args: Vec<_> = env::args().skip(1).collect();
+mod install_manpages;
 
-    match args.as_slice() {
-        [cmd, path] if cmd == "install-manpages" => install_manpages(Path::new(path)),
-        _ => todo!(),
-    }
+#[enum_dispatch]
+pub trait Subcommand {
+    fn run(&self) -> Result<()>;
 }
 
-fn install_manpages(path: &Path) -> Result<()> {
-    use clap::CommandFactory;
-    use clap_mangen::Man;
-    use std::fs::File;
-    use std::io::BufWriter;
+#[enum_dispatch(Subcommand)]
+#[derive(Parser)]
+pub enum SubcommandEnum {
+    InstallManpages(install_manpages::Opts),
+}
 
-    fn render_manpages(path: &Path, cmd: &clap::Command, version: &str) -> Result<()> {
-        let name = cmd
-            .get_bin_name()
-            .unwrap_or(cmd.get_name())
-            .replace(" ", "-");
+#[derive(Parser)]
+#[command(about, arg_required_else_help = true, disable_help_subcommand = true)]
+pub struct Opts {
+    #[command(subcommand)]
+    pub subcommand: SubcommandEnum,
+}
 
-        Man::new(cmd.clone())
-            // pass space, otherwise the ordering of the remaining arguments is incorrect
-            .date(" ")
-            .manual("PineAPPL CLI Manual")
-            .source(format!("PineAPPL {version}"))
-            .title(name.to_ascii_uppercase())
-            .render(&mut BufWriter::new(File::create(
-                path.join(format!("{name}.1")),
-            )?))?;
+fn main() -> Result<()> {
+    let opts = Opts::parse();
 
-        for subcmd in cmd.get_subcommands() {
-            render_manpages(path, subcmd, version)?;
-        }
-
-        Ok(())
-    }
-
-    let cmd = pineappl_cli::Opts::command();
-    let version: String = cmd
-        .get_version()
-        // UNWRAP: the command must have a version
-        .unwrap()
-        .strip_prefix('v')
-        // UNWRAP: the version string must start with a 'v'
-        .unwrap()
-        .to_string();
-    let mut cmd = cmd.version(version.clone());
-
-    // this is needed so subcommands return the correct `bin_name`
-    cmd.build();
-
-    render_manpages(path, &cmd, &version)?;
-
-    Ok(())
+    opts.subcommand.run()
 }
