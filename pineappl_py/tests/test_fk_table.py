339d90837d6efc0cb54719fd8c7e50b53076b7a5
Alessandro Candido <candido.ale@gmail.com>
2024-04-26 21:56:12 +0200

    test: Compelte fk table test adaption from the grid one

--- pineappl_py/tests/test_fk_table.py
+++ pineappl_py/tests/test_fk_table.py
@@ -5,11 +5,12 @@ import pineappl
 
 class TestFkTable:
     def fake_grid(self, bins=None):
-        lumis = [pineappl.lumi.LumiEntry([(1, 21, 0.1)])]
+        lumis = [pineappl.lumi.LumiEntry([(1, 21, 1.0)])]
         orders = [pineappl.grid.Order(0, 0, 0, 0)]
         bin_limits = np.array([1e-7, 1e-3, 1] if bins is None else bins, dtype=float)
         subgrid_params = pineappl.subgrid.SubgridParams()
         g = pineappl.grid.Grid.create(lumis, orders, bin_limits, subgrid_params)
+        g.set_key_value("lumi_id_types", "pdg_mc_ids")
         return g
 
     def test_convolute_with_one(self):
@@ -27,14 +28,10 @@ class TestFkTable:
         g.set_subgrid(0, 0, 0, subgrid)
         fk = pineappl.fk_table.FkTable.from_grid(g)
         np.testing.assert_allclose(
-            fk.convolute_with_one(2212, lambda pid, x, q2: 0.0, lambda q2: 0.0),
+            fk.convolute_with_one(2212, lambda pid, x, q2: 0.0),
             [0.0] * 2,
         )
         np.testing.assert_allclose(
-            fk.convolute_with_one(2212, lambda pid, x, q2: 1, lambda q2: 1.0),
-            [5e6 / 9999, 0.0],
-        )
-        np.testing.assert_allclose(
-            fk.convolute_with_one(2212, lambda pid, x, q2: 1, lambda q2: 2.0),
-            [2**3 * 5e6 / 9999, 0.0],
+            fk.convolute_with_one(2212, lambda pid, x, q2: 1),
+            [5e7 / 9999, 0.0],
         )
