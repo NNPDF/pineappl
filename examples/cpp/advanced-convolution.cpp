9ded95b07e0bdc0b7ce7f2e0c1300a88fbcca825
Radonirinaunimi <rrabeman@nikhef.nl>
2025-04-08 12:44:29 +0200

    Fix memory leak in `advanced-convolution.cpp`

--- examples/cpp/advanced-convolution.cpp
+++ examples/cpp/advanced-convolution.cpp
@@ -126,11 +126,13 @@ int main(int argc, char* argv[]) {
     // modify the particle ID representation of the Grid
     char* ref_pid_repr_basis = pineappl_get_pid_basis(grid);
     assert(std::string(ref_pid_repr_basis) == "PINEAPPL_PID_BASIS_EVOL");
+    pineappl_string_delete(ref_pid_repr_basis);
 
     pineappl_rotate_pid_basis(grid, PINEAPPL_PID_BASIS_PDG);
 
     char* mod_pid_repr_basis = pineappl_get_pid_basis(grid);
     assert(std::string(mod_pid_repr_basis) == "PINEAPPL_PID_BASIS_PDG");
+    pineappl_string_delete(mod_pid_repr_basis);
 
     pineappl_grid_delete(grid);
 }
