770b76309084d064f6f876f3a6119fff2be0445c
Radonirinaunimi <rrabeman@nikhef.nl>
2025-04-11 11:10:44 +0200

    Add `unravel_index` and tests to example

--- examples/cpp/get-subgrids.cpp
+++ examples/cpp/get-subgrids.cpp
@@ -1,12 +1,43 @@
 #include <pineappl_capi.h>
 
+#include <cassert>
 #include <cstddef>
 #include <iomanip>
 #include <ios>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
 #include <numeric>
+
+
+std::vector<std::size_t> unravel_index(std::size_t flat_index, const std::vector<std::size_t>& shape) {
+    std::size_t ndim = shape.size();
+    std::vector<std::size_t> coords(ndim);
+
+    for (int i = ndim - 1; i >= 0; --i) {
+        coords[i] = flat_index % shape[i];
+        flat_index /= shape[i];
+    }
+
+    return coords;
+}
+
+std::string coords_to_string(const std::vector<std::size_t>& coords) {
+    std::ostringstream osstream;
+
+    osstream << "(";
+    for (std::size_t i = 0; i < coords.size(); ++i) {
+        osstream << coords[i];
+        if (i != coords.size() - 1) {
+            osstream << ", ";
+        }
+    }
+    osstream << ")";
+
+    return osstream.str();
+}
+
 
 int main() {
     std::string filename = "drell-yan-rap-ll.pineappl.lz4";
@@ -23,9 +54,11 @@ int main() {
     std::size_t subgrid_dim = pineappl_grid_kinematics_len(grid);
 
     std::cout << std::right << std::setw(10) << "bin" << std::setw(10) << "sg idx"
-        << std::setw(16) << "sg value" << "\n";
+        << std::setw(6 * subgrid_dim) << "sg coordinates" << std::setw(16)
+        << "sg value" << "\n";
     std::cout << std::right << std::setw(10) << "---" << std::setw(10) << "------"
-        << std::setw(16) << "------------" << "\n";
+        << std::setw(6 * subgrid_dim) << "--------------" << std::setw(16)
+        << "------------" << "\n";
 
     for (std::size_t b = 0; b < n_bins; ++b) {
         std::vector<std::size_t> subgrid_shape(subgrid_dim);
@@ -39,11 +72,19 @@ int main() {
 
             pineappl_grid_subgrid_array(grid, b, order, channel, subgrid_array.data());
 
-            // TODO: Unravel of the index & check against some reference values
-            for (std::size_t value = 0; value < subgrid_array.size(); ++value) {
-                if (subgrid_array[value] != 0) {
+            for (std::size_t index = 0; index < subgrid_array.size(); ++index) {
+                if (subgrid_array[index] != 0) {
+                    // Unravel the index to recover the standard coordinates
+                    std::vector<std::size_t> coords = unravel_index(index, subgrid_shape);
+
                     std::cout << std::right << std::setw(10) << b << std::setw(10)
-                        << value << std::setw(16) << subgrid_array[value] << "\n";
+                        << index << std::setw(6 * subgrid_dim) << coords_to_string(coords)
+                        << std::setw(16) << subgrid_array[index] << "\n";
+
+                    // Compare to some reference value
+                    if (b==0 && index==41020) {
+                        assert(subgrid_array[index] == -4.936156925096015e-07);
+                    }
                     break;
                 }
             }
