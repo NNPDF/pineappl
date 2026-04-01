083521e80dbd08d3e3e97ebc9d7a5f2b9f7d7649
Christopher Schwan <handgranaten-herbert@posteo.de>
2026-04-01 12:14:01 +0200

    Deduplicate code

--- pineappl/src/v0.rs
+++ pineappl/src/v0.rs
@@ -41,10 +41,21 @@ pub fn default_interps(flexible_scale: bool, convolutions: usize) -> Vec<Interp>
 }
 
 pub fn read_uncompressed_v0(mut reader: impl BufRead) -> Result<Grid> {
+    use pineappl_v0::convolutions::Convolution;
     use pineappl_v0::subgrid::Subgrid as _;
 
     let grid = GridV0::read(&mut reader).map_err(|err| Error::Other(err.into()))?;
-    let convolutions = read_convolutions_from_metadata(&grid);
+    let convolutions: Vec<_> = grid
+        .convolutions()
+        .into_iter()
+        .map(|old_conv| match old_conv {
+            Convolution::UnpolPDF(pid) => Some(Conv::new(ConvType::UnpolPDF, pid)),
+            Convolution::PolPDF(pid) => Some(Conv::new(ConvType::PolPDF, pid)),
+            Convolution::UnpolFF(pid) => Some(Conv::new(ConvType::UnpolFF, pid)),
+            Convolution::PolFF(pid) => Some(Conv::new(ConvType::PolFF, pid)),
+            Convolution::None => None,
+        })
+        .collect();
 
     let flexible_scale_grid = grid.subgrids().iter().any(|subgrid| {
         subgrid
@@ -260,75 +271,4 @@ pub fn read_uncompressed_v0(mut reader: impl BufRead) -> Result<Grid> {
     assert_eq!(result.bwfl().len(), grid.bin_info().bins());
 
     Ok(result)
-}
-
-fn read_convolutions_from_metadata(grid: &GridV0) -> Vec<Option<Conv>> {
-    grid.key_values().map_or_else(
-        // if there isn't any metadata, we assume two unpolarized proton-PDFs are used
-        || vec![Some(Conv::new(ConvType::UnpolPDF, 2212)); 2],
-        |kv| {
-            // file format v0 only supports exactly two convolutions
-            (1..=2)
-                .map(|index| {
-                    // if there are key-value pairs `convolution_particle_1` and
-                    // `convolution_type_1` and the same with a higher index, we convert this
-                    // metadata into `Convolution`
-                    match (
-                        kv.get(&format!("convolution_particle_{index}"))
-                            .map(|s| s.parse::<i32>()),
-                        kv.get(&format!("convolution_type_{index}"))
-                            .map(String::as_str),
-                    ) {
-                        (_, Some("None")) => None,
-                        (Some(Ok(pid)), Some("UnpolPDF")) => Some(Conv::new(ConvType::UnpolPDF, pid)),
-                        (Some(Ok(pid)), Some("PolPDF")) => Some(Conv::new(ConvType::PolPDF, pid)),
-                        (Some(Ok(pid)), Some("UnpolFF")) => Some(Conv::new(ConvType::UnpolFF, pid)),
-                        (Some(Ok(pid)), Some("PolFF")) => Some(Conv::new(ConvType::PolFF, pid)),
-                        (None, None) => {
-                            // if these key-value pairs are missing use the old metadata
-                            match kv
-                                .get(&format!("initial_state_{index}"))
-                                .map(|s| s.parse::<i32>())
-                            {
-                                Some(Ok(pid)) => {
-                                    // Addresses: https://github.com/NNPDF/pineappl/issues/334
-                                    if grid.channels().is_empty() && pid == 2212 {
-                                        Some(Conv::new(ConvType::UnpolPDF, 2212))
-                                    } else {
-                                        let condition = !grid.channels().iter().all(|entry| {
-                                            entry.entry().iter().all(|&(a, b, _)|
-                                                match index {
-                                                    1 => a,
-                                                    2 => b,
-                                                    _ => unreachable!()
-                                                } == pid
-                                            )
-                                        });
-
-                                        condition.then_some(Conv::new(ConvType::UnpolPDF, pid))
-                                    }
-                                }
-                                None => Some(Conv::new(ConvType::UnpolPDF, 2212)),
-                                Some(Err(err)) => panic!(
-                                    "metadata 'initial_state_{index}' could not be parsed: {err}"
-                                ),
-                            }
-                        }
-                        (None, Some(_)) => {
-                            panic!("metadata 'convolution_type_{index}' is missing")
-                        }
-                        (Some(_), None) => {
-                            panic!("metadata 'convolution_particle_{index}' is missing")
-                        }
-                        (Some(Ok(_)), Some(type_)) => {
-                            panic!("metadata 'convolution_type_{index} = {type_}' is unknown")
-                        }
-                        (Some(Err(err)), Some(_)) => {
-                            panic!("metadata 'convolution_particle_{index}' could not be parsed: {err}")
-                        }
-                    }
-                })
-                .collect()
-        },
-    )
 }
